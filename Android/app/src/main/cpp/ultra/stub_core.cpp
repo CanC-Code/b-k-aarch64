d509f66c106b4911d432364b10ceee4305b812e8
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-16 21:41:27 -0700

    Update stub_core.cpp

--- Android/app/src/main/cpp/ultra/stub_core.cpp
+++ Android/app/src/main/cpp/ultra/stub_core.cpp
@@ -1,5 +1,5 @@
-// Purpose: Temporary stubs for core1/core2/audio functions to allow Android build
-// Author: CCVO
+// File: Android/app/src/main/cpp/ultra/stub_core.cpp
+// Purpose: Temporary stubs for core/audio functions to allow Android build
 
 #include <cstdint>
 #include <cstddef>
@@ -8,22 +8,30 @@ extern "C" {
 
 // ---- Core 1 ----
 void core1_reset(uint8_t* ram) {
-    // Stub: do nothing
+    // stub
 }
 
 void core1_stepCPU(uint8_t* ram) {
-    // Stub: do nothing
+    // stub
 }
 
 // ---- Core 2 ----
 void core2_stepFrame(uint8_t* ram, uint32_t* framebuffer, int width, int height) {
-    // Stub: do nothing
+    // stub
 }
 
 // ---- Audio ----
 void n_audioInit() {
-    // Stub: do nothing
+    // stub
 }
 
 void n_audioStep() {
-    // 
+    // stub
+}
+
+void n_audioGetBuffer(int16_t* buffer, size_t samples) {
+    if (!buffer) return;
+    for (size_t i = 0; i < samples; i++) buffer[i] = 0; // silence
+}
+
+} // extern "C"
