06ede4a0ec0dc1b30cdf1e192bf60afbde683440
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-17 22:25:15 -0700

    Create otr_assets.h

---
+++ Android/app/src/main/cpp/ultra/otr_assets.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstddef>
+
+namespace OTRAssets {
+    extern const char* us_v10_yaml;
+    extern const size_t us_v10_size;
+
+    extern const char* pal_yaml;
+    extern const size_t pal_size;
+}
