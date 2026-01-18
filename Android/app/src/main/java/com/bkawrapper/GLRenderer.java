df2bf56e1f1aafcc4c37164e285199d54eed500f
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-18 16:14:07 -0700

    Update GLRenderer.java

--- Android/app/src/main/java/com/bkawrapper/GLRenderer.java
+++ Android/app/src/main/java/com/bkawrapper/GLRenderer.java
@@ -2,65 +2,50 @@ package com.bkawrapper;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.FrameLayout;
+import android.content.Context;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class GLRenderer implements GLSurfaceView.Renderer {
 
-    private final GLSurfaceView glSurfaceView;
-    private final ProgressBar progressBar;
-    private final TextView progressText;
-    private final FrameLayout overlay;
+    private byte[] otrData;
+    private final Object lock = new Object();
+    private final Context context;
 
-    private byte[] otrData = null;
-    private boolean otrInitialized = false;
-
-    public GLRenderer(GLSurfaceView glSurfaceView,
-                      ProgressBar progressBar,
-                      TextView progressText,
-                      FrameLayout overlay) {
-        this.glSurfaceView = glSurfaceView;
-        this.progressBar = progressBar;
-        this.progressText = progressText;
-        this.overlay = overlay;
+    public GLRenderer(Context ctx) {
+        context = ctx;
     }
 
     public void setOTRData(byte[] data) {
-        this.otrData = data;
-        this.otrInitialized = false;
-
-        if (otrData != null) {
-            // Queue texture init on GL thread
-            glSurfaceView.queueEvent(() -> {
-                NativeBridge.initTextureWithOTR(otrData);
-                otrInitialized = true;
-            });
-
-            // Hide overlay on UI thread
-            overlay.post(() -> overlay.setVisibility(FrameLayout.GONE));
+        synchronized (lock) {
+            otrData = data;
         }
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(0f, 0f, 0f, 1f);
+        GLES20.glClearColor(0f,0f,0f,1f);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        GLES20.glViewport(0, 0, width, height);
+        GLES20.glViewport(0,0,width,height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
-        if (otrData != null && otrInitialized) {
-            NativeBridge.updateTexture(NativeBridge.getTextureId());
+        synchronized (lock) {
+            if (otrData != null) {
+                // TODO: decode OTR to texture and render
+                // Safe: always on GL thread
+            }
         }
     }
 }
