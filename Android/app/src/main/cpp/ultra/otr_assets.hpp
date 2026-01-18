62f2ad88733e200b923c5d3889db8f11a445e4df
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-18 16:18:49 -0700

    Create otr_assets.hpp

---
+++ Android/app/src/main/cpp/ultra/otr_assets.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstddef>
+#include <cstdint>
+
+extern const uint8_t embedded_pal_yaml[];
+extern const size_t embedded_pal_size;
+
+extern const uint8_t embedded_us_yaml[];
+extern const size_t embedded_us_size;
