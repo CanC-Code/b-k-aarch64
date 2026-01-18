1f0332cdb66d11c9fc5d3507cf84e6f0316380b4
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-18 16:20:47 -0700

    Update otr_assets.hpp

--- Android/app/src/main/cpp/ultra/otr_assets.hpp
+++ Android/app/src/main/cpp/ultra/otr_assets.hpp
@@ -2,8 +2,9 @@
 #include <cstddef>
 #include <cstdint>
 
+// YAML data embedded as C++ arrays
 extern const uint8_t embedded_pal_yaml[];
-extern const size_t embedded_pal_size;
+extern const size_t  embedded_pal_size;
 
 extern const uint8_t embedded_us_yaml[];
-extern const size_t embedded_us_size;
+extern const size_t  embedded_us_size;
