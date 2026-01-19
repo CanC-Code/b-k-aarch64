32411ed1cd75ec29c1fd830e6d606ca31f52dd1a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 09:58:07 -0700

    Update Menu.java

--- Android/app/src/main/java/com/bkawrapper/Menu.java
+++ Android/app/src/main/java/com/bkawrapper/Menu.java
@@ -1,34 +1,22 @@
 package com.bkawrapper;
 
 import android.app.Activity;
-import android.view.View;
 
 public class Menu {
 
-    private final Activity activity;
-
     public Menu(Activity activity) {
-        this.activity = activity;
-        NativeBridge.nativeInitMenu(activity);
+        MenuNative.nativeInitMenu(activity);
     }
 
     public void toggleMenu() {
-        NativeBridge.nativeOnBackPressed();
+        MenuNative.nativeToggleMenu();
     }
 
     public void pauseEmulator() {
-        NativeBridge.nativePauseEmulator();
+        MenuNative.nativePauseEmulator();
     }
 
     public void resumeEmulator() {
-        NativeBridge.nativeResumeEmulator();
-    }
-
-    public void showMenu() {
-        NativeBridge.nativeOnBackPressed(); // ensures menu is visible
-    }
-
-    public void hideMenu() {
-        NativeBridge.nativeOnBackPressed(); // toggles menu hidden
+        MenuNative.nativeResumeEmulator();
     }
 }
