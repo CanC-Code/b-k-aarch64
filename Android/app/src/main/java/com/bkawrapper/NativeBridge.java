54bfe972737beaf72cf77df28e1875e8234c556a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 02:22:50 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -1,26 +1,36 @@
 package com.bkawrapper;
 
-import android.app.Activity;
 import android.content.ContentResolver;
 import android.net.Uri;
 
 public class NativeBridge {
 
-    // ------------------------------------------------------------
-    // ROM / OTR
-    // ------------------------------------------------------------
-    public static native void loadRomFromUri(ContentResolver resolver, Uri uri);
-    public static native void initTexture();
-    public static native void startGameLoop();
-    public static native void stopGameLoop();
-    public static native void cleanupGame();
-    public static native float getOTRProgress();
+    static {
+        System.loadLibrary("wrapper"); // Match your CMake/NDK library
+    }
 
-    // ------------------------------------------------------------
-    // Menu / Emulator control
-    // ------------------------------------------------------------
-    public static native void nativeInitMenu(Activity activity);
+    /* ===========================
+       ROM / OTR
+       =========================== */
+    public static native void loadRomFromUri(ContentResolver resolver, Uri uri);
+    public static native float getOTRProgress();
+    public static native void saveOTRToFile(String path);
+
+    /* ===========================
+       Game loop
+       =========================== */
+    public static native void startGameLoop();
+    public static native void cleanupGame();
+
+    /* ===========================
+       Menu
+       =========================== */
+    public static native void nativeInitMenu(Object activity);
     public static native void nativeOnBackPressed();
-    public static native void nativePauseEmulator();
-    public static native void nativeResumeEmulator();
+
+    /* ===========================
+       GL / Texture
+       =========================== */
+    public static native void initTexture();
+    public static native void updateTexture(int textureId);
 }
