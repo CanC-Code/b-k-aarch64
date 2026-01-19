2ed52a938f1d6988882d63624c29bee8340f1b07
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 01:49:43 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -1,69 +1,26 @@
-// File: Android/app/src/main/java/com/bkawrapper/NativeBridge.java
 package com.bkawrapper;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.net.Uri;
-import android.view.Surface;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+public class NativeBridge {
 
-public final class NativeBridge {
-
-    static {
-        System.loadLibrary("wrapper");
-    }
-
-    private NativeBridge() {}
-
-    // -------- Native API --------
-    public static native void loadRom(byte[] rom);
-    public static native void processRom();
-
-    public static native void initGame(Surface surface);
-    public static native void cleanupGame();
-
+    // ------------------------------------------------------------
+    // ROM / OTR
+    // ------------------------------------------------------------
+    public static native void loadRomFromUri(ContentResolver resolver, Uri uri);
+    public static native void initTexture();
     public static native void startGameLoop();
     public static native void stopGameLoop();
+    public static native void cleanupGame();
+    public static native float getOTRProgress();
 
-    public static native int initTexture();
-    public static native void updateTexture(int texId);
-
-    // -------- OTR access --------
-    public static native byte[] getOTRData();
-    public static native void saveOTRToFile(String path);
-
-    // -------- OTR Progress --------
-    public static native float getOTRProgress(); // 0.0 to 1.0
-
-    // -------- SAF Loader --------
-    public static void loadRomFromUri(ContentResolver resolver, Uri uri) throws Exception {
-        try (InputStream is = resolver.openInputStream(uri);
-             ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-
-            if (is == null) throw new Exception("InputStream null");
-
-            byte[] buf = new byte[8192];
-            int r;
-            while ((r = is.read(buf)) > 0) bos.write(buf, 0, r);
-
-            byte[] rom = bos.toByteArray();
-            if (rom.length < 0x1000) throw new Exception("ROM too small");
-
-            loadRom(rom);
-            processRom();
-        }
-    }
-
-    // -------- Helper: Save OTR to file path (Java side convenience) --------
-    public static void saveOTRToPath(String path) throws Exception {
-        byte[] otr = getOTRData();
-        if (otr == null || otr.length == 0) throw new Exception("OTR empty");
-
-        try (FileOutputStream fos = new FileOutputStream(path)) {
-            fos.write(otr);
-            fos.flush();
-        }
-    }
+    // ------------------------------------------------------------
+    // Menu / Emulator control
+    // ------------------------------------------------------------
+    public static native void nativeInitMenu(Activity activity);
+    public static native void nativeOnBackPressed();
+    public static native void nativePauseEmulator();
+    public static native void nativeResumeEmulator();
 }
