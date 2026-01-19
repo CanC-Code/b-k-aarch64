2a4d17cb410896779a1d84efa2dc9fff0cc94f39
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 01:17:52 -0700

    Update menu.hpp

--- Android/app/src/main/cpp/menu/menu.hpp
+++ Android/app/src/main/cpp/menu/menu.hpp
@@ -8,11 +8,13 @@ public:
 
     void showMenu();
     void hideMenu();
+    void toggleMenu();      // NEW
 
 private:
     JavaVM* vm_;
     jobject activityGlobal_;
     jobject menuOverlayGlobal_;
+    bool visible_ = false; // track menu state
 
     void setVisibility(bool visible);
 };
