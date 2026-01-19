409cbab15ff375ad53480ad15bd2faf1d4557242
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 00:50:30 -0700

    Update menu.cpp

--- Android/app/src/main/cpp/menu/menu.cpp
+++ Android/app/src/main/cpp/menu/menu.cpp
@@ -14,11 +14,7 @@ MenuHandler::MenuHandler(JavaVM* vm, jobject activity)
 
     jclass activityCls = env->GetObjectClass(activity);
     jfieldID menuField =
-        env->GetFieldID(
-            activityCls,
-            "menuOverlay",
-            "Landroid/widget/LinearLayout;"
-        );
+        env->GetFieldID(activityCls, "menuOverlay", "Landroid/widget/LinearLayout;");
 
     jobject menuLocal = env->GetObjectField(activity, menuField);
     menuOverlayGlobal_ = env->NewGlobalRef(menuLocal);
@@ -34,39 +30,27 @@ MenuHandler::~MenuHandler() {
     env->DeleteGlobalRef(activityGlobal_);
 }
 
-void MenuHandler::setVisibilityOnUiThread(bool visible) {
+void MenuHandler::setVisibility(bool visible) {
     JNIEnv* env = nullptr;
     vm_->AttachCurrentThread(&env, nullptr);
-
-    jclass activityCls = env->GetObjectClass(activityGlobal_);
-    jmethodID runOnUiThread =
-        env->GetMethodID(
-            activityCls,
-            "runOnUiThread",
-            "(Ljava/lang/Runnable;)V"
-        );
-
-    jclass runnableCls = env->FindClass("java/lang/Runnable");
-
-    jobject runnable = env->NewObject(
-        runnableCls,
-        env->GetMethodID(runnableCls, "<init>", "()V")
-    );
 
     jclass viewCls = env->GetObjectClass(menuOverlayGlobal_);
     jmethodID setVis =
         env->GetMethodID(viewCls, "setVisibility", "(I)V");
 
-    jint visibility = visible ? 0 : 8; // VISIBLE / GONE
-    env->CallVoidMethod(menuOverlayGlobal_, setVis, visibility);
+    env->CallVoidMethod(
+        menuOverlayGlobal_,
+        setVis,
+        visible ? 0 /* View.VISIBLE */ : 8 /* View.GONE */
+    );
 }
 
 void MenuHandler::showMenu() {
     LOGI("showMenu()");
-    setVisibilityOnUiThread(true);
+    setVisibility(true);
 }
 
 void MenuHandler::hideMenu() {
     LOGI("hideMenu()");
-    setVisibilityOnUiThread(false);
+    setVisibility(false);
 }
