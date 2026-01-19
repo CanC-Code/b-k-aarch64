03aebf16ad570d7a992557f73477f0885f3ac9b7
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-18 21:56:12 -0700

    Update otr_builder.cpp

--- Android/app/src/main/cpp/ultra/otr_builder.cpp
+++ Android/app/src/main/cpp/ultra/otr_builder.cpp
@@ -1,21 +1,25 @@
 #include "otr_generator.hpp"
-#include "otr_assets.hpp"
-#include <vector>
-#include <cstdint>
+#include <android/asset_manager.h>
+#include <android/log.h>
 
-bool generateOTRFromROM(const uint8_t* romData, size_t romSize, float& outProgress) {
-    OTRGenerator generator;
+#define LOG_TAG "OTR_BUILDER"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-    // Load embedded YAML assets
-    generator.loadEmbeddedYAML("decompressed.pal.yaml", embedded_pal_yaml, embedded_pal_yaml_size);
-    generator.loadEmbeddedYAML("decompressed.us.v10.yaml", embedded_us_yaml, embedded_us_yaml_size);
+void buildOTR(AAssetManager* assetManager) {
+    OTRGenerator generator(assetManager);
 
-    // Generate OTR directly from ROM
-    bool success = generator.generate(
-        romData,
-        romSize,
-        [&](float progress) { outProgress = progress; }
-    );
+    // Load YAML files from assets
+    auto palData = generator.readAsset(assetManager, "otr_yaml/decompressed.pal.yaml");
+    auto usData  = generator.readAsset(assetManager, "otr_yaml/decompressed.us.v10.yaml");
 
-    return success;
+    generator.loadYAML("pal", palData.data(), palData.size());
+    generator.loadYAML("us.v10", usData.data(), usData.size());
+
+    std::vector<uint8_t> outPal, outUS;
+    if (generator.generate("pal", outPal)) {
+        LOGI("PAL OTR generated, size: %zu", outPal.size());
+    }
+    if (generator.generate("us.v10", outUS)) {
+        LOGI("US OTR generated, size: %zu", outUS.size());
+    }
 }
