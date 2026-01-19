8d619a02a328558300623d1db7d651f2dddd2e11
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 01:50:37 -0700

    Update Menu.java

--- Android/app/src/main/java/com/bkawrapper/Menu.java
+++ Android/app/src/main/java/com/bkawrapper/Menu.java
@@ -1,60 +1,34 @@
 package com.bkawrapper;
 
 import android.app.Activity;
-import android.view.MotionEvent;
 import android.view.View;
 
 public class Menu {
 
     private final Activity activity;
-    private final View menuOverlay;
-    private boolean visible = false;
-
-    private float swipeStartX = -1;
-    private float swipeStartY = -1;
 
     public Menu(Activity activity) {
         this.activity = activity;
-        this.menuOverlay = activity.findViewById(R.id.menu_overlay);
-        hideMenu(); // start hidden
+        NativeBridge.nativeInitMenu(activity);
     }
 
-    public void handleBackPressed() {
-        if (!visible) {
-            showMenu();
-            NativeBridge.nativePauseEmulator();
-        } else {
-            hideMenu();
-            NativeBridge.nativeResumeEmulator();
-        }
+    public void toggleMenu() {
+        NativeBridge.nativeOnBackPressed();
     }
 
-    public boolean handleTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                swipeStartX = event.getX();
-                swipeStartY = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                if (swipeStartX < 200 && swipeStartY < 200) { // top-left corner
-                    float dy = event.getY() - swipeStartY;
-                    if (dy > 150) { // swipe down threshold
-                        handleBackPressed();
-                        return true;
-                    }
-                }
-                break;
-        }
-        return false;
+    public void pauseEmulator() {
+        NativeBridge.nativePauseEmulator();
     }
 
-    private void showMenu() {
-        activity.runOnUiThread(() -> menuOverlay.setVisibility(View.VISIBLE));
-        visible = true;
+    public void resumeEmulator() {
+        NativeBridge.nativeResumeEmulator();
     }
 
-    private void hideMenu() {
-        activity.runOnUiThread(() -> menuOverlay.setVisibility(View.GONE));
-        visible = false;
+    public void showMenu() {
+        NativeBridge.nativeOnBackPressed(); // ensures menu is visible
+    }
+
+    public void hideMenu() {
+        NativeBridge.nativeOnBackPressed(); // toggles menu hidden
     }
 }
