64019e1909006ea589fa7d6c33f938c005eab8a8
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-18 21:55:43 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/NativeBridge.cpp
+++ Android/app/src/main/cpp/NativeBridge.cpp
@@ -1,51 +1,47 @@
 #include <jni.h>
-#include <vector>
-#include <mutex>
-#include <android/asset_manager_jni.h>
 #include "otr_generator.hpp"
-#include "GLRenderer.hpp"
+#include <vector>
+#include <android/asset_manager_jni.h>
+#include <android/log.h>
 
-static std::unique_ptr<OTRGenerator> g_otrGenerator;
-static std::mutex g_mutex;
-static std::vector<uint8_t> g_romData;
+#define LOG_TAG "NATIVE_BRIDGE"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 extern "C" {
 
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_initOTRGenerator(JNIEnv* env, jobject /*thiz*/, jobject assetManager) {
-    std::lock_guard<std::mutex> lock(g_mutex);
-    if (!g_otrGenerator) {
-        g_otrGenerator = std::make_unique<OTRGenerator>(AAssetManager_fromJava(env, assetManager));
+std::vector<uint8_t> readAsset(AAssetManager* mgr, const char* path) {
+    AAsset* asset = AAssetManager_open(mgr, path, AASSET_MODE_BUFFER);
+    if (!asset) {
+        LOGI("Failed to open asset: %s", path);
+        return {};
     }
+    size_t size = AAsset_getLength(asset);
+    std::vector<uint8_t> buffer(size);
+    AAsset_read(asset, buffer.data(), size);
+    AAsset_close(asset);
+    return buffer;
 }
 
 JNIEXPORT jboolean JNICALL
-Java_com_bkawrapper_NativeBridge_loadROM(JNIEnv* env, jobject /*thiz*/, jbyteArray romBytes) {
-    std::lock_guard<std::mutex> lock(g_mutex);
-    jsize len = env->GetArrayLength(romBytes);
-    g_romData.resize(len);
-    env->GetByteArrayRegion(romBytes, 0, len, reinterpret_cast<jbyte*>(g_romData.data()));
-    return JNI_TRUE;
+Java_com_bkawrapper_NativeBridge_generateOTR(JNIEnv* env, jobject thiz, jobject assetManager) {
+    AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
+    if (!mgr) return JNI_FALSE;
+
+    OTRGenerator generator(mgr);
+
+    auto palData = readAsset(mgr, "otr_yaml/decompressed.pal.yaml");
+    auto usData  = readAsset(mgr, "otr_yaml/decompressed.us.v10.yaml");
+
+    generator.loadYAML("pal", palData.data(), palData.size());
+    generator.loadYAML("us.v10", usData.data(), usData.size());
+
+    std::vector<uint8_t> outPal;
+    std::vector<uint8_t> outUS;
+
+    bool success1 = generator.generate("pal", outPal);
+    bool success2 = generator.generate("us.v10", outUS);
+
+    return (success1 && success2) ? JNI_TRUE : JNI_FALSE;
 }
 
-JNIEXPORT jboolean JNICALL
-Java_com_bkawrapper_NativeBridge_generateOTR(JNIEnv* env, jobject /*thiz*/) {
-    std::lock_guard<std::mutex> lock(g_mutex);
-    if (!g_otrGenerator || g_romData.empty()) return JNI_FALSE;
-
-    bool success = g_otrGenerator->generateOTR(g_romData);
-    if (success) {
-        GLRenderer::getInstance().setOTRData(g_otrGenerator->getOTRBuffer());
-    }
-    return success ? JNI_TRUE : JNI_FALSE;
 }
-
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_clearOTR(JNIEnv* env, jobject /*thiz*/) {
-    std::lock_guard<std::mutex> lock(g_mutex);
-    g_romData.clear();
-    if (g_otrGenerator) g_otrGenerator->clear();
-    GLRenderer::getInstance().clear();
-}
-
-} // extern "C"
