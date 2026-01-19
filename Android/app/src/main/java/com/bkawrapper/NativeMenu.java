af397684d21fb44bd0cd1241b95144067217c81a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 12:02:08 -0700

    Update NativeMenu.java

--- Android/app/src/main/java/com/bkawrapper/NativeMenu.java
+++ Android/app/src/main/java/com/bkawrapper/NativeMenu.java
@@ -5,11 +5,18 @@ import android.view.View;
 public class NativeMenu {
 
     static {
-        System.loadLibrary("emulator_native");
+        System.loadLibrary("wrapper"); // your JNI library
     }
 
+    /** Initialize menu (called from MenuController) */
     public static native void nativeInitMenu(View menuOverlay);
+
+    /** Called on back button or swipe gesture */
     public static native void nativeOnBackPressed();
+
+    /** Pause emulator loop */
     public static native void nativePauseEmulator();
+
+    /** Resume emulator loop */
     public static native void nativeResumeEmulator();
 }
