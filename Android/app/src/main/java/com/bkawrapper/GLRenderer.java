36c3a3ab8a8ffd1da1a0e015dc419190485e5f3d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-18 17:57:41 -0700

    Update GLRenderer.java

--- Android/app/src/main/java/com/bkawrapper/GLRenderer.java
+++ Android/app/src/main/java/com/bkawrapper/GLRenderer.java
@@ -1,8 +1,8 @@
 package com.bkawrapper;
 
+import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.content.Context;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -13,39 +13,46 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class GLRenderer implements GLSurfaceView.Renderer {
 
+    private Context context;
     private byte[] otrData;
-    private final Object lock = new Object();
-    private final Context context;
 
-    public GLRenderer(Context ctx) {
-        context = ctx;
+    // Simple triangle for demo
+    private FloatBuffer vertexBuffer;
+    private final float[] vertices = {
+            0f, 0.5f, 0f,
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f
+    };
+
+    public GLRenderer(Context context) {
+        this.context = context;
+
+        // Setup vertex buffer
+        ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(vertices);
+        vertexBuffer.position(0);
     }
 
     public void setOTRData(byte[] data) {
-        synchronized (lock) {
-            otrData = data;
-        }
+        this.otrData = data;
+        // TODO: Upload OTR texture to OpenGL
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(0f,0f,0f,1f);
+        GLES20.glClearColor(0f, 0f, 0f, 1f);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        GLES20.glViewport(0,0,width,height);
+        GLES20.glViewport(0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-
-        synchronized (lock) {
-            if (otrData != null) {
-                // TODO: decode OTR to texture and render
-                // Safe: always on GL thread
-            }
-        }
+        // TODO: Render OTR texture if available
     }
 }
