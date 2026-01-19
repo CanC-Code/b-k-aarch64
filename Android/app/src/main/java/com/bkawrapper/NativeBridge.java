53e68c92ddbe818065ba96414e758211e485edd9
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 09:59:36 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -1,36 +1,13 @@
 package com.bkawrapper;
 
-import android.content.ContentResolver;
-import android.net.Uri;
+import android.app.Activity;
 
 public class NativeBridge {
 
     static {
-        System.loadLibrary("wrapper"); // Match your CMake/NDK library
+        System.loadLibrary("bkawrapper");
     }
 
-    /* ===========================
-       ROM / OTR
-       =========================== */
-    public static native void loadRomFromUri(ContentResolver resolver, Uri uri);
-    public static native float getOTRProgress();
-    public static native void saveOTRToFile(String path);
-
-    /* ===========================
-       Game loop
-       =========================== */
-    public static native void startGameLoop();
-    public static native void cleanupGame();
-
-    /* ===========================
-       Menu
-       =========================== */
-    public static native void nativeInitMenu(Object activity);
-    public static native void nativeOnBackPressed();
-
-    /* ===========================
-       GL / Texture
-       =========================== */
-    public static native void initTexture();
-    public static native void updateTexture(int textureId);
+    public static native void nativeInit(Activity activity);
+    public static native void stopGameLoop();
 }
