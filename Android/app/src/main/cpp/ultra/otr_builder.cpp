a64f1a1c339fd150424a4bc5becac8d96b26f7b8
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-18 22:07:42 -0700

    Update otr_builder.cpp

--- Android/app/src/main/cpp/ultra/otr_builder.cpp
+++ Android/app/src/main/cpp/ultra/otr_builder.cpp
@@ -1,25 +1,20 @@
-#include "otr_generator.hpp"
-#include <android/asset_manager.h>
-#include <android/log.h>
+// File: Android/app/src/main/cpp/ultra/otr_builder.cpp
+// Purpose: Builds OTR data using assets loaded via NativeBridge
 
-#define LOG_TAG "OTR_BUILDER"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#include "otr_builder.hpp"
+#include "otr_generator.hpp"
+#include "NativeBridge.hpp" // Provides readAsset
+#include <android/asset_manager_jni.h>
+#include <vector>
+#include <cstdint>
 
 void buildOTR(AAssetManager* assetManager) {
-    OTRGenerator generator(assetManager);
+    OTRGenerator generator;
 
-    // Load YAML files from assets
-    auto palData = generator.readAsset(assetManager, "otr_yaml/decompressed.pal.yaml");
-    auto usData  = generator.readAsset(assetManager, "otr_yaml/decompressed.us.v10.yaml");
+    // Load palettes and YAML data using the global readAsset function
+    auto palData = readAsset(assetManager, "otr_yaml/decompressed.pal.yaml");
+    auto usData  = readAsset(assetManager, "otr_yaml/decompressed.us.v10.yaml");
 
-    generator.loadYAML("pal", palData.data(), palData.size());
-    generator.loadYAML("us.v10", usData.data(), usData.size());
-
-    std::vector<uint8_t> outPal, outUS;
-    if (generator.generate("pal", outPal)) {
-        LOGI("PAL OTR generated, size: %zu", outPal.size());
-    }
-    if (generator.generate("us.v10", outUS)) {
-        LOGI("US OTR generated, size: %zu", outUS.size());
-    }
+    // Pass loaded data to the generator
+    generator.generateOTR(palData, usData);
 }
