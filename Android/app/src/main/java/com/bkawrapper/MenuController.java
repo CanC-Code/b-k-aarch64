c5c4c5ac0f09dde804e492b603d5b9cc6854a67a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 16:41:56 -0700

    Update MenuController.java

--- Android/app/src/main/java/com/bkawrapper/MenuController.java
+++ Android/app/src/main/java/com/bkawrapper/MenuController.java
@@ -1,53 +1,33 @@
-package com.bkawrapper;
+package com.bkawrapper.menu;
 
-import android.app.Activity;
-import android.view.MotionEvent;
-import android.view.View;
+public class MenuController {
 
-public class MenuController implements View.OnTouchListener {
+    private final MenuOverlayView menuView;
+    private boolean visible = false;
 
-    private static final int SWIPE_THRESHOLD = 120;
-
-    private float startX;
-    private float startY;
-
-    private final Activity activity;
-
-    public MenuController(Activity activity) {
-        this.activity = activity;
+    public MenuController(MenuOverlayView menuView) {
+        this.menuView = menuView;
     }
 
-    public static void attach(Activity activity, MenuController controller) {
-        View decorView = activity.getWindow().getDecorView();
-        decorView.setOnTouchListener(controller);
-    }
-
-    /**
-     * Called from MainActivity.onBackPressed()
-     */
-    public void onBackPressed() {
-        NativeMenu.nativeOnBackPressed();
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getActionMasked()) {
-            case MotionEvent.ACTION_DOWN:
-                startX = event.getX();
-                startY = event.getY();
-                break;
-
-            case MotionEvent.ACTION_UP:
-                float dx = event.getX() - startX;
-                float dy = event.getY() - startY;
-
-                // Swipe down from top-left corner
-                if (startX < 200 && startY < 200 && dy > SWIPE_THRESHOLD) {
-                    NativeMenu.nativeOnBackPressed();
-                    return true;
-                }
-                break;
+    public void toggle() {
+        if (visible) {
+            hide();
+        } else {
+            show();
         }
-        return false;
+    }
+
+    public void show() {
+        visible = true;
+        menuView.show();
+    }
+
+    public void hide() {
+        visible = false;
+        menuView.hide();
+    }
+
+    public boolean isVisible() {
+        return visible;
     }
 }
