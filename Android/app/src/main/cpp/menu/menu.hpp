9540d6c28b73230e5381a2e2ecdc4f909698efaf
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-20 01:22:22 -0700

    Update menu.hpp

--- Android/app/src/main/cpp/menu/menu.hpp
+++ Android/app/src/main/cpp/menu/menu.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include <jni.h>
-#include <atomic>
 
 class MenuHandler {
 public:
     MenuHandler(JavaVM* vm, jobject activity);
     ~MenuHandler();
 
-    void showMenu();   // show menu and pause emulator
-    void hideMenu();   // hide menu and resume emulator
+    void showMenu();
+    void hideMenu();
     void toggleVisibility();
     bool isVisible() const;
 
@@ -16,7 +15,7 @@ private:
     JavaVM* vm_;
     jobject activityGlobal_;
     jobject menuOverlayGlobal_;
-    std::atomic<bool> visible_{false};
+    bool visible_ = false;
 
     void setVisibility(bool visible);
 };
