3494717b47a16269fc4c7706e334717f458b36fd
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 20:27:36 -0700

    Update NativeMenu.java

--- Android/app/src/main/java/com/bkawrapper/NativeMenu.java
+++ Android/app/src/main/java/com/bkawrapper/NativeMenu.java
@@ -2,9 +2,8 @@
 package com.bkawrapper;
 
 public class NativeMenu {
-    /** Toggles the menu via JNI */
-    public static native void nativeToggleMenu();
 
+    public static native void nativeToggleMenu();
     public static native void nativePauseEmulator();
     public static native void nativeResumeEmulator();
 }
