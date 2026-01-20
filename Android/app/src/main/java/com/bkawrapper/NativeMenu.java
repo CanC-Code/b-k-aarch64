510a3522c0a49c73105312b159b0dabe9a158ca2
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-20 01:31:40 -0700

    Delete NativeMenu.java

--- Android/app/src/main/java/com/bkawrapper/NativeMenu.java
+++
@@ -1,9 +0,0 @@
-// File: Android/app/src/main/java/com/bkawrapper/NativeMenu.java
-package com.bkawrapper;
-
-public class NativeMenu {
-
-    public static native void nativeToggleMenu();
-    public static native void nativePauseEmulator();
-    public static native void nativeResumeEmulator();
-}
