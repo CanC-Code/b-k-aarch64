402079ae2fb801c70aeabf8d9abb9e2cfe83f8e9
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 18:09:02 -0700

    Update GLRenderer.java

--- Android/app/src/main/java/com/bkawrapper/GLRenderer.java
+++ Android/app/src/main/java/com/bkawrapper/GLRenderer.java
@@ -1,93 +1,22 @@
-// File: Android/app/src/main/java/com/bkawrapper/GLRenderer.java
 package com.bkawrapper;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.opengl.Matrix;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
-
-import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class GLRenderer implements GLSurfaceView.Renderer {
 
-    private final MainActivity activity;
-
-    private int program;
-    private int textureId = 0;
-    private boolean textureReady = false;
-
-    private FloatBuffer vertexBuffer;
-    private FloatBuffer texBuffer;
-    private final float[] mvp = new float[16];
-
-    private final float[] verts = {
-            -1,  1, 0,
-            -1, -1, 0,
-             1,  1, 0,
-             1, -1, 0
-    };
-
-    private final float[] tex = {
-            0, 0,
-            0, 1,
-            1, 0,
-            1, 1
-    };
-
-    public GLRenderer(MainActivity activity) {
-        this.activity = activity;
-    }
+    private int textureId;
 
     @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(0, 0, 0, 1);
-        Matrix.setIdentityM(mvp, 0);
-
-        vertexBuffer = ByteBuffer.allocateDirect(verts.length * 4)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer();
-        vertexBuffer.put(verts).position(0);
-
-        texBuffer = ByteBuffer.allocateDirect(tex.length * 4)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer();
-        texBuffer.put(tex).position(0);
-
-        program = buildProgram();
-
-        activity.onSurfaceReady();
+    public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
+        // setup texture
+        textureId = createTexture();
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-
-        if (!textureReady) return;
-
         NativeBridge.updateTexture(textureId);
-
-        GLES20.glUseProgram(program);
-
-        int p = GLES20.glGetAttribLocation(program, "aPos");
-        int t = GLES20.glGetAttribLocation(program, "aUV");
-        int m = GLES20.glGetUniformLocation(program, "uMVP");
-        int s = GLES20.glGetUniformLocation(program, "uTex");
-
-        GLES20.glEnableVertexAttribArray(p);
-        GLES20.glVertexAttribPointer(p, 3, GLES20.GL_FLOAT, false, 0, vertexBuffer);
-
-        GLES20.glEnableVertexAttribArray(t);
-        GLES20.glVertexAttribPointer(t, 2, GLES20.GL_FLOAT, false, 0, texBuffer);
-
-        GLES20.glUniformMatrix4fv(m, 1, false, mvp, 0);
-        GLES20.glUniform1i(s, 0);
-
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
-
-        GLES20.glDisableVertexAttribArray(p);
-        GLES20.glDisableVertexAttribArray(t);
     }
 
     @Override
@@ -95,40 +24,9 @@ public class GLRenderer implements GLSurfaceView.Renderer {
         GLES20.glViewport(0, 0, width, height);
     }
 
-    private int buildProgram() {
-        String vs =
-                "uniform mat4 uMVP;" +
-                "attribute vec4 aPos;" +
-                "attribute vec2 aUV;" +
-                "varying vec2 vUV;" +
-                "void main(){gl_Position=uMVP*aPos;vUV=aUV;}";
-
-        String fs =
-                "precision mediump float;" +
-                "uniform sampler2D uTex;" +
-                "varying vec2 vUV;" +
-                "void main(){gl_FragColor=texture2D(uTex,vUV);}";
-
-        int v = compile(GLES20.GL_VERTEX_SHADER, vs);
-        int f = compile(GLES20.GL_FRAGMENT_SHADER, fs);
-
-        int p = GLES20.glCreateProgram();
-        GLES20.glAttachShader(p, v);
-        GLES20.glAttachShader(p, f);
-        GLES20.glLinkProgram(p);
-        return p;
-    }
-
-    private int compile(int type, String src) {
-        int s = GLES20.glCreateShader(type);
-        GLES20.glShaderSource(s, src);
-        GLES20.glCompileShader(s);
-        return s;
-    }
-
-    /** Called ONLY by MainActivity once native core is ready */
-    public void attachTexture(int texId) {
-        this.textureId = texId;
-        this.textureReady = texId != 0;
+    private int createTexture() {
+        int[] textures = new int[1];
+        GLES20.glGenTextures(1, textures, 0);
+        return textures[0];
     }
 }
