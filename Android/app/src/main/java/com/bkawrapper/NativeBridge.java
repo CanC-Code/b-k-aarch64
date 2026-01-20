e8a23498694c6d038742f057b102a368729b3262
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 17:23:19 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -1,32 +1,11 @@
 package com.bkawrapper;
 
-import com.bkawrapper.menu.MenuController;
-
-public class NativeBridge {
+public final class NativeBridge {
 
     static {
         System.loadLibrary("bk_native");
     }
 
-    private static MenuController menuController;
-
-    public static void setMenuController(MenuController controller) {
-        menuController = controller;
-    }
-
-    // Called from native
-    public static void showMenu() {
-        if (menuController != null) {
-            menuController.show();
-        }
-    }
-
-    public static void hideMenu() {
-        if (menuController != null) {
-            menuController.hide();
-        }
-    }
-
-    public static native void nativeResumeGame();
-    public static native void nativeQuitGame();
+    public static native void pauseGameLoop();
+    public static native void resumeGameLoop();
 }
