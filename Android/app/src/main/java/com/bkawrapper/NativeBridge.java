033d78b05c96180e6411baab9f7b5e9fee325fc9
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-20 01:31:24 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -1,4 +1,3 @@
-// File: Android/app/src/main/java/com/bkawrapper/NativeBridge.java
 package com.bkawrapper;
 
 import android.app.Activity;
@@ -6,19 +5,12 @@ import android.app.Activity;
 public class NativeBridge {
 
     static {
-        System.loadLibrary("bk_wrapper");
+        System.loadLibrary("bkawrapper");
     }
 
-    // Game loop control
-    public static native void startGameLoop();
-    public static native void pauseGameLoop();
-    public static native void resumeGameLoop();
-    public static native void cleanupGame();
-
-    // Menu
     public static native void nativeInitMenu(Activity activity);
     public static native void nativeOnBackPressed();
 
-    // Texture update (stub)
-    public static native void updateTexture(int textureId);
+    public static native void startGameLoop();
+    public static native void cleanupGame();
 }
