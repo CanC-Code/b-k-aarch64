5671bebc2b909e20dabe14b107444fa30cb762eb
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 17:46:47 -0700

    Update menu.hpp

--- Android/app/src/main/cpp/menu/menu.hpp
+++ Android/app/src/main/cpp/menu/menu.hpp
@@ -1,3 +1,4 @@
+// File: Android/app/src/main/cpp/menu/menu.hpp
 #pragma once
 #include <jni.h>
 
@@ -6,9 +7,11 @@ public:
     MenuHandler(JavaVM* vm, jobject activity);
     ~MenuHandler();
 
-    void showMenu();   // show menu and pause emulator
-    void hideMenu();   // hide menu and resume emulator
+    void showMenu();
+    void hideMenu();
+    void toggleVisibility();
     bool isVisible() const;
+    jobject getOverlay() const; // for wrapper loop pause check
 
 private:
     JavaVM* vm_;
