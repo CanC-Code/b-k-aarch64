69282cc9d381b3c9be2873c3341fb40adf10be3c
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 19:43:19 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -1,23 +1,13 @@
+// File: Android/app/src/main/java/com/bkawrapper/NativeBridge.java
 package com.bkawrapper;
 
-public class NativeBridge {
+import android.app.Activity;
 
+public class NativeBridge {
     static {
-        System.loadLibrary("bk_wrapper"); // matches your CMake target
+        System.loadLibrary("wrapper"); // matches your CMake target
     }
 
-    // Core/game methods
-    public static native void nativeInitCore();
-    public static native void nativeOnCoreStep();
-
-    // Menu methods
-    public static native void nativeInitMenu();
-    public static native boolean nativeOnBackPressed();
-
-    // New JNI hooks for MainActivity
-    public static native void pauseGameLoop();
-    public static native void resumeGameLoop();
-
-    // Called from GLRenderer
-    public static native void updateTexture(int textureId);
+    /** Initialize the native menu (passes activity to JNI) */
+    public static native void nativeInitMenu(Activity activity);
 }
