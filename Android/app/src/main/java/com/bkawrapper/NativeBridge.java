0786cefea302279ff8e64ac2e037fde71dd342bf
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 17:29:26 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -6,6 +6,15 @@ public final class NativeBridge {
         System.loadLibrary("bk_native");
     }
 
+    // Menu control
     public static native void pauseGameLoop();
     public static native void resumeGameLoop();
+
+    // Renderer hooks (required by GLRenderer)
+    public static native void updateTexture(int textureId);
+
+    // Optional quit hook (safe stub)
+    public static void nativeQuitGame() {
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
 }
