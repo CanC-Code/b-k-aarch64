3812bded34e934a36aca2a0b75e0a900f6ffcb41
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 17:23:43 -0700

    Update wrapper.cpp

--- Android/app/src/main/cpp/wrapper.cpp
+++ Android/app/src/main/cpp/wrapper.cpp
@@ -1,91 +1,32 @@
 #include <jni.h>
-#include <android/log.h>
 #include <atomic>
+#include <unistd.h>
 
-// Menu system
-#include "menu/menu.hpp"
-
-// Emulator stubs (include path already points at /emulator)
-#include "stubs.h"
-#include "texture_stubs.h"
-
-// Ultra / OTR
-#include "ultra/otr_builder.h"
-
-#define LOG_TAG "BKA_WRAPPER"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-
-/*
- * Global state
- */
 static std::atomic<bool> g_paused{false};
-static MenuHandler* g_menu = nullptr;
 
 extern "C" {
 
-/*
- * JNI: Menu initialization
- */
+// Called from Java
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeMenu_nativeInitMenu(
-        JNIEnv* env,
-        jclass,
-        jobject menuView) {
-
-    if (!g_menu) {
-        g_menu = new MenuHandler(env, menuView);
-        LOGI("Menu initialized");
-    }
+Java_com_bkawrapper_NativeBridge_pauseGameLoop(JNIEnv*, jclass) {
+    g_paused.store(true, std::memory_order_release);
 }
 
-/*
- * JNI: Back press / toggle menu
- */
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeMenu_nativeOnBackPressed(
-        JNIEnv*,
-        jclass) {
-
-    if (!g_menu) {
-        return;
-    }
-
-    if (g_menu->isVisible()) {
-        g_menu->hide();
-        g_paused.store(false);
-    } else {
-        g_menu->show();
-        g_paused.store(true);
-    }
+Java_com_bkawrapper_NativeBridge_resumeGameLoop(JNIEnv*, jclass) {
+    g_paused.store(false, std::memory_order_release);
 }
 
-/*
- * JNI: Pause emulator
- */
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeMenu_nativePauseEmulator(
-        JNIEnv*,
-        jclass) {
-
-    g_paused.store(true);
-    LOGI("Emulator paused");
 }
 
-/*
- * JNI: Resume emulator
- */
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeMenu_nativeResumeEmulator(
-        JNIEnv*,
-        jclass) {
+// Example emulation loop hook
+void emulator_main_loop() {
+    while (true) {
+        if (g_paused.load(std::memory_order_acquire)) {
+            usleep(16 * 1000); // ~60Hz idle
+            continue;
+        }
 
-    if (g_menu) {
-        g_menu->hide();
+        // ---- emulator frame step here ----
     }
-
-    g_paused.store(false);
-    LOGI("Emulator resumed");
 }
-
-} // extern "C"
