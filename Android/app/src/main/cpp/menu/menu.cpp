b8f2de1b06f6087deb922bf0d79d1cc6787c7d2d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-20 01:10:54 -0700

    Update menu.cpp

--- Android/app/src/main/cpp/menu/menu.cpp
+++ Android/app/src/main/cpp/menu/menu.cpp
@@ -1,56 +1,72 @@
 #include "menu.hpp"
 #include <android/log.h>
-#include <unistd.h>
 
 #define LOG_TAG "MENU_HANDLER"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-// ------------------------------------------------------------
-static MenuHandler* g_menu = nullptr;
-static JavaVM* g_vm = nullptr;
-
-MenuHandler::MenuHandler(JavaVM* vm, jobject activity) : vm_(vm) {
+MenuHandler::MenuHandler(JavaVM* vm, jobject activity)
+    : vm_(vm)
+{
     JNIEnv* env = nullptr;
     vm_->GetEnv((void**)&env, JNI_VERSION_1_6);
+
+    // Store global references
     activityGlobal_ = env->NewGlobalRef(activity);
 
-    jclass activityCls = env->GetObjectClass(activityGlobal_);
-    jfieldID menuField = env->GetFieldID(activityCls, "menu_overlay", "Landroid/widget/LinearLayout;");
-    menuOverlayGlobal_ = env->NewGlobalRef(env->GetObjectField(activityGlobal_, menuField));
+    jclass activityCls = env->GetObjectClass(activity);
+    jfieldID overlayField = env->GetFieldID(activityCls, "menuOverlay", "Landroid/widget/LinearLayout;");
+    menuOverlayGlobal_ = env->NewGlobalRef(env->GetObjectField(activity, overlayField));
+
+    visible_ = false;
 }
 
 MenuHandler::~MenuHandler() {
-    if (!vm_) return;
+    if (!vm_ || !activityGlobal_) return;
+
     JNIEnv* env = nullptr;
     vm_->AttachCurrentThread(&env, nullptr);
-    env->DeleteGlobalRef(activityGlobal_);
-    env->DeleteGlobalRef(menuOverlayGlobal_);
+    if (menuOverlayGlobal_) env->DeleteGlobalRef(menuOverlayGlobal_);
+    if (activityGlobal_) env->DeleteGlobalRef(activityGlobal_);
 }
 
 void MenuHandler::setVisibility(bool visible) {
     JNIEnv* env = nullptr;
     vm_->AttachCurrentThread(&env, nullptr);
 
-    jclass viewCls = env->GetObjectClass(menuOverlayGlobal_);
-    jmethodID setVis = env->GetMethodID(viewCls, "setVisibility", "(I)V");
-    env->CallVoidMethod(menuOverlayGlobal_, setVis, visible ? 0 /* VISIBLE */ : 8 /* GONE */);
+    jclass overlayCls = env->GetObjectClass(menuOverlayGlobal_);
+    jmethodID setVis = env->GetMethodID(overlayCls, "setVisibility", "(I)V");
+    env->CallVoidMethod(menuOverlayGlobal_, setVis, visible ? 0 : 8); // VISIBLE : GONE
 
-    visible_ = visible;
+    visible_.store(visible);
 }
 
-void MenuHandler::showMenu() { setVisibility(true); }
-void MenuHandler::hideMenu() { setVisibility(false); }
-void MenuHandler::toggleVisibility() { isVisible() ? hideMenu() : showMenu(); }
-bool MenuHandler::isVisible() const { return visible_; }
+void MenuHandler::showMenu() {
+    setVisibility(true);
+}
+
+void MenuHandler::hideMenu() {
+    setVisibility(false);
+}
+
+void MenuHandler::toggleVisibility() {
+    if (isVisible()) hideMenu();
+    else showMenu();
+}
+
+bool MenuHandler::isVisible() const {
+    return visible_.load();
+}
 
 // ------------------------------------------------------------
-// JNI hooks
+// JNI Hooks
 extern "C" {
+
+static MenuHandler* g_menu = nullptr;
+static JavaVM* g_vm = nullptr;
 
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_NativeBridge_nativeInitMenu(JNIEnv* env, jclass, jobject activity) {
-    if (!g_menu)
-        g_menu = new MenuHandler(g_vm, activity);
+    if (!g_menu) g_menu = new MenuHandler(g_vm, activity);
 }
 
 JNIEXPORT void JNICALL
