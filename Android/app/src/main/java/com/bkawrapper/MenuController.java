b26dfe56f5a3eb9e99eed200b19cba9f76dc191a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-19 18:08:48 -0700

    Update MenuController.java

--- Android/app/src/main/java/com/bkawrapper/MenuController.java
+++ Android/app/src/main/java/com/bkawrapper/MenuController.java
@@ -1,53 +1,34 @@
 package com.bkawrapper;
 
-import android.app.Activity;
-import android.view.MotionEvent;
-import android.view.View;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import androidx.activity.ComponentActivity;
 import androidx.activity.OnBackPressedCallback;
 
 public class MenuController {
 
-    private final Activity activity;
-    private float swipeStartX = -1;
-    private float swipeStartY = -1;
+    private final ComponentActivity activity;
 
-    public MenuController(Activity activity) {
+    public MenuController(ComponentActivity activity) {
         this.activity = activity;
-        NativeBridge.nativeInitMenu(activity);
+        setupBackHandler();
     }
 
-    public void onBackPressed() {
-        NativeBridge.nativeOnBackPressed();
-    }
-
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                swipeStartX = event.getX();
-                swipeStartY = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                if (swipeStartX < 200 && swipeStartY < 200) {
-                    float dy = event.getY() - swipeStartY;
-                    if (dy > 150) {
-                        NativeBridge.nativeOnBackPressed();
-                        return true;
-                    }
-                }
-                break;
-        }
-        return false;
-    }
-
-    public static void attach(Activity activity, MenuController menuController) {
+    private void setupBackHandler() {
         activity.getOnBackPressedDispatcher().addCallback(activity, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-                menuController.onBackPressed();
+                boolean handled = NativeBridge.nativeOnBackPressed();
+                if (!handled) {
+                    setEnabled(false);
+                    activity.onBackPressed();
+                    setEnabled(true);
+                }
             }
         });
+    }
 
-        View root = activity.getWindow().getDecorView();
-        root.setOnTouchListener((v, event) -> menuController.onTouchEvent(event));
+    public void initMenu() {
+        NativeBridge.nativeInitMenu();
     }
 }
