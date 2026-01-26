e10bbc559107fe57ca6bda77950db6cc3eccf9cd
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-25 22:24:38 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -10,12 +10,14 @@ public class NativeBridge {
 
     public static native void nativeInit(Object activity);
     public static native void startGameLoop();
-    public static native void pauseGameLoop();
-    public static native void resumeGameLoop();
     public static native void cleanupGame();
     public static native void loadRomFromUri(ContentResolver resolver, Uri uri);
-
-    // Added to resolve GLRenderer compilation error
+    
+    // Texture stubs for GLRenderer
     public static native int initTexture();
     public static native void updateTexture(int textureId);
+
+    // Required by MainActivity even if empty
+    public static native void pauseGameLoop();
+    public static native void resumeGameLoop();
 }
