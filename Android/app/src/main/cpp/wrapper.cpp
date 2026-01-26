486a02a0a3095abf387ff55576efe2cdc28e7f2c
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-25 22:24:14 -0700

    Update wrapper.cpp

--- Android/app/src/main/cpp/wrapper.cpp
+++ Android/app/src/main/cpp/wrapper.cpp
@@ -4,12 +4,12 @@
 #include <vector>
 #include <android/log.h>
 #include <unistd.h>
-#include <fcntl.h>
 
 #define LOG_TAG "BK_WRAPPER"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 static std::atomic<bool> g_running{false};
+static std::atomic<bool> g_rom_loaded{false};
 static std::thread g_thread;
 
 extern "C" {
@@ -21,30 +21,25 @@ extern "C" {
 static std::vector<uint8_t> g_ram(8 * 1024 * 1024, 0);
 
 void emuLoop() {
-    core1_reset(g_ram.data());
-    while (g_running.load()) {
-        n_audioStep();
-        usleep(16000);
+    // Wait until ROM is actually ready
+    while(!g_rom_loaded.load() && g_running.load()) {
+        usleep(100000); 
+    }
+
+    if (g_running.load()) {
+        core1_reset(g_ram.data());
+        LOGI("Emulator Loop Active");
+        while (g_running.load()) {
+            n_audioStep();
+            usleep(16000);
+        }
     }
 }
 
 extern "C" {
 
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject activity) {
-    LOGI("Native Bridge Initialized");
-}
-
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_startGameLoop(JNIEnv* env, jclass clazz) {
-    if (!g_running.load()) {
-        g_running.store(true);
-        g_thread = std::thread(emuLoop);
-    }
-}
-
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_loadRomFromUri(JNIEnv* env, jclass clazz, jobject resolver, jobject uri) {
+Java_com_bkawrapper_NativeBridge_loadRomFromUri(JNIEnv* env, jclass, jobject resolver, jobject uri) {
     jclass resCls = env->GetObjectClass(resolver);
     jmethodID openDoc = env->GetMethodID(resCls, "openFileDescriptor", "(Landroid/net/Uri;Ljava/lang/String;)Landroid/os/ParcelFileDescriptor;");
     jstring mode = env->NewStringUTF("r");
@@ -54,15 +49,32 @@ Java_com_bkawrapper_NativeBridge_loadRomFromUri(JNIEnv* env, jclass clazz, jobje
         jclass pfdCls = env->GetObjectClass(pfd);
         jmethodID getFd = env->GetMethodID(pfdCls, "getFd", "()I");
         int fd = env->CallIntMethod(pfd, getFd);
+        
         core1_loadOTR(fd);
+        g_rom_loaded.store(true);
     }
 }
 
-JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_pauseGameLoop(JNIEnv*, jclass) { g_running.store(false); }
-JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_resumeGameLoop(JNIEnv*, jclass) { /* Not implemented */ }
+JNIEXPORT void JNICALL
+Java_com_bkawrapper_NativeBridge_startGameLoop(JNIEnv*, jclass) {
+    if (!g_running.load()) {
+        g_running.store(true);
+        g_thread = std::thread(emuLoop);
+    }
+}
+
+JNIEXPORT void JNICALL
+Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv*, jclass, jobject) {
+    LOGI("Native Bridge Ready");
+}
+
 JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_cleanupGame(JNIEnv*, jclass) {
     g_running.store(false);
     if(g_thread.joinable()) g_thread.join();
 }
+
+// Stubs for the GLRenderer to keep it from crashing
+JNIEXPORT jint JNICALL Java_com_bkawrapper_NativeBridge_initTexture(JNIEnv*, jclass) { return 1; }
+JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_updateTexture(JNIEnv*, jclass, jint) {}
 
 }
