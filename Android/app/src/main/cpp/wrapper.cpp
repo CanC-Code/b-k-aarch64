11f011c3ba9ce1d0a043982afcc0faaf464568bb
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-25 20:35:44 -0700

    Update wrapper.cpp

--- Android/app/src/main/cpp/wrapper.cpp
+++ Android/app/src/main/cpp/wrapper.cpp
@@ -1,9 +1,11 @@
+// File: Android/app/src/main/cpp/wrapper.cpp
 #include <jni.h>
 #include <thread>
 #include <atomic>
 #include <vector>
 #include <android/log.h>
-#include <unistd.h>
+#include <unistd.h> // Fixed: Required for usleep
+#include <fcntl.h>
 #include "menu/menu.hpp"
 
 #define LOG_TAG "BK_WRAPPER"
@@ -14,14 +16,16 @@ static MenuHandler* g_menu = nullptr;
 static std::atomic<bool> g_running{false};
 static std::thread g_thread;
 
-extern "C" void n_audioStep();
-extern "C" void core1_reset(uint8_t*);
+extern "C" {
+    void n_audioStep();
+    void core1_reset(uint8_t*);
+    void core1_loadOTR(int fd); // Assumed signature for asset loading
+}
 
 static std::vector<uint8_t> g_ram(8 * 1024 * 1024);
 
 static void emuLoop() {
     core1_reset(g_ram.data());
-
     while (g_running.load()) {
         if (!g_menu || !g_menu->isVisible()) {
             n_audioStep();
@@ -33,8 +37,7 @@ static void emuLoop() {
 extern "C" {
 
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_nativeInitMenu(
-        JNIEnv* env, jclass, jobject activity) {
+Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass, jobject activity) {
     if (!g_menu) {
         g_menu = new MenuHandler(g_vm, activity);
         LOGI("Menu initialized");
@@ -42,21 +45,30 @@ Java_com_bkawrapper_NativeBridge_nativeInitMenu(
 }
 
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_nativeOnBackPressed(
-        JNIEnv*, jclass) {
-    if (g_menu) g_menu->toggleVisibility();
+Java_com_bkawrapper_NativeBridge_loadRomFromUri(JNIEnv* env, jclass, jobject resolver, jobject uri) {
+    jclass resCls = env->GetObjectClass(resolver);
+    jmethodID openDoc = env->GetMethodID(resCls, "openFileDescriptor", "(Landroid/net/Uri;Ljava/lang/String;)Landroid/os/ParcelFileDescriptor;");
+    jstring mode = env->NewStringUTF("r");
+    jobject pfd = env->CallObjectMethod(resolver, openDoc, uri, mode);
+
+    if (pfd) {
+        jclass pfdCls = env->GetObjectClass(pfd);
+        jmethodID getFd = env->GetMethodID(pfdCls, "getFd", "()I");
+        int fd = env->CallIntMethod(pfd, getFd);
+        core1_loadOTR(fd);
+    }
 }
 
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_startGameLoop(
-        JNIEnv*, jclass) {
-    g_running.store(true);
-    g_thread = std::thread(emuLoop);
+Java_com_bkawrapper_NativeBridge_startGameLoop(JNIEnv*, jclass) {
+    if (!g_running.load()) {
+        g_running.store(true);
+        g_thread = std::thread(emuLoop);
+    }
 }
 
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_cleanupGame(
-        JNIEnv*, jclass) {
+Java_com_bkawrapper_NativeBridge_cleanupGame(JNIEnv*, jclass) {
     g_running.store(false);
     if (g_thread.joinable()) g_thread.join();
 }
@@ -64,7 +76,6 @@ Java_com_bkawrapper_NativeBridge_cleanupGame(
 JNIEXPORT jint JNICALL
 JNI_OnLoad(JavaVM* vm, void*) {
     g_vm = vm;
-    LOGI("JNI_OnLoad OK");
     return JNI_VERSION_1_6;
 }
 
