64990e86704f4c946c6cfc327d6c62f04d360ac4
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-26 20:37:53 -0700

    Update assets_manifest.h

--- Android/app/src/main/cpp/ultra/assets_manifest.h
+++ Android/app/src/main/cpp/ultra/assets_manifest.h
@@ -1,33 +1,28 @@
-// Android/app/src/main/cpp/ultra/assets_manifest.h
 #ifndef ASSETS_MANIFEST_H
 #define ASSETS_MANIFEST_H
 
-#include <stdint.h>
+#include <cstdint>
 
-#pragma pack(push, 1)
-
-// Maps to the 'file_type_to_asset_type' logic in generate_asset_enums.py
-enum AssetType : uint8_t {
-    ASSET_TYPE_ANIM   = 0,
-    ASSET_TYPE_MODEL  = 1,
-    ASSET_TYPE_SPRITE = 2,
-    ASSET_TYPE_DIALOG = 3,
-    ASSET_TYPE_SKIP   = 255 
+enum AssetType {
+    ASSET_TYPE_SKIP = 0,
+    ASSET_TYPE_COMPRESSED = 1,
+    ASSET_TYPE_RAW = 2
 };
 
+#pragma pack(push, 1)
 struct AssetEntry {
-    uint32_t uid;           // Asset ID (e.g. 0x1516)
-    uint32_t romOffset;     [span_1](start_span)// Calculated offset in the ROM[span_1](end_span)
-    uint32_t compSize;      // Size to read from ROM
+    uint32_t romOffset;     // Calculated offset in the ROM
+    uint32_t compSize;      // Size of data in ROM (compressed)
     uint32_t decompSize;    // Expected output size
-    uint8_t  type;          // AssetType enum
-    char name[32];          [span_2](start_span)// Human-readable name[span_2](end_span)
+    uint32_t type;          // AssetType
+    char name[32];          // Human-readable name
 };
 
 struct ManifestHeader {
-    uint32_t magic;         // 'BKAR'
-    uint32_t entryCount;    [span_3](start_span)//[span_3](end_span)
+    uint32_t magic;         // e.g., 0x424B414D 'BKAM'
+    uint32_t entryCount;    // Total number of assets
+    uint32_t version;       // Version of the manifest format
 };
-
 #pragma pack(pop)
+
 #endif
