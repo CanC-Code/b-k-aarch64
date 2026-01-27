aca9fd68ef3702d8a146cd2af6a6a0afbd1a5792
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 11:28:52 -0700

    Update assets_manifest.h

--- Android/app/src/main/cpp/ultra/assets_manifest.h
+++ Android/app/src/main/cpp/ultra/assets_manifest.h
@@ -1,28 +1,21 @@
 #ifndef ASSETS_MANIFEST_H
 #define ASSETS_MANIFEST_H
 
-#include <cstdint>
+#include <stdint.h>
 
-enum AssetType {
-    ASSET_TYPE_SKIP = 0,
-    ASSET_TYPE_COMPRESSED = 1,
-    ASSET_TYPE_RAW = 2
-};
+#define ASSET_TYPE_SKIP 0
+#define ASSET_TYPE_RAW  1
 
-#pragma pack(push, 1)
-struct AssetEntry {
-    uint32_t romOffset;     // Calculated offset in the ROM
-    uint32_t compSize;      // Size of data in ROM (compressed)
-    uint32_t decompSize;    // Expected output size
-    uint32_t type;          // AssetType
-    char name[32];          // Human-readable name
-};
+typedef struct {
+    uint32_t romOffset;
+    uint32_t compSize;
+    uint32_t type;
+    char name[128];
+} AssetEntry;
 
-struct ManifestHeader {
-    uint32_t magic;         // e.g., 0x424B414D 'BKAM'
-    uint32_t entryCount;    // Total number of assets
-    uint32_t version;       // Version of the manifest format
-};
-#pragma pack(pop)
+typedef struct {
+    uint32_t magic;      // e.g., 0x424B4101
+    uint32_t entryCount;
+} ManifestHeader;
 
 #endif
