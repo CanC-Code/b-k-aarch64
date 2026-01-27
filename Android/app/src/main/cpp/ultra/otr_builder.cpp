d24f4a144bb53f3c7d71e186aea6dbe7dc068f9e
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-26 23:44:51 -0700

    Update otr_builder.cpp

--- Android/app/src/main/cpp/ultra/otr_builder.cpp
+++ Android/app/src/main/cpp/ultra/otr_builder.cpp
@@ -11,12 +11,12 @@
 #define LOG_TAG "OTR_BUILDER"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
+// Shared JVM pointer
 static JavaVM* g_jvm = nullptr;
 
-// Automatically called when library loads
-JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+// Helper to receive the JVM from wrapper.cpp
+void otr_builder_set_jvm(JavaVM* vm) {
     g_jvm = vm;
-    return JNI_VERSION_1_6;
 }
 
 void process_asset(int romFd, AssetEntry& asset, const char* outDirPath) {
@@ -48,7 +48,6 @@ void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmet
     ManifestHeader* header = (ManifestHeader*)manifestPtr;
     AssetEntry* entries = (AssetEntry*)(manifestPtr + sizeof(ManifestHeader));
 
-    // CRITICAL: activity is a local ref; worker threads need a GlobalRef
     jobject globalActivity = env->NewGlobalRef(activity);
 
     const int batchSize = 4;
@@ -61,22 +60,23 @@ void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmet
 
         for (auto& w : workers) if (w.joinable()) w.join();
 
-        // UI CALLBACK: Must ensure thread is attached to VM
         JNIEnv* myEnv;
         bool attached = false;
-        int res = g_jvm->GetEnv((void**)&myEnv, JNI_VERSION_1_6);
-        if (res == JNI_EDETACHED) {
-            if (g_jvm->AttachCurrentThread(&myEnv, NULL) == JNI_OK) attached = true;
-        }
+        if (g_jvm) {
+            int res = g_jvm->GetEnv((void**)&myEnv, JNI_VERSION_1_6);
+            if (res == JNI_EDETACHED) {
+                if (g_jvm->AttachCurrentThread(&myEnv, NULL) == JNI_OK) attached = true;
+            }
 
-        if (myEnv && globalActivity && progressMid) {
-            int percent = (int)((float)(i + 1) / header->entryCount * 100.0f);
-            jstring jName = myEnv->NewStringUTF(entries[i].name);
-            myEnv->CallVoidMethod(globalActivity, progressMid, percent, jName);
-            myEnv->DeleteLocalRef(jName);
-        }
+            if (myEnv && globalActivity && progressMid) {
+                int percent = (int)((float)(i + 1) / header->entryCount * 100.0f);
+                jstring jName = myEnv->NewStringUTF(entries[i].name);
+                myEnv->CallVoidMethod(globalActivity, progressMid, percent, jName);
+                myEnv->DeleteLocalRef(jName);
+            }
 
-        if (attached) g_jvm->DetachCurrentThread();
+            if (attached) g_jvm->DetachCurrentThread();
+        }
     }
     
     env->DeleteGlobalRef(globalActivity);
