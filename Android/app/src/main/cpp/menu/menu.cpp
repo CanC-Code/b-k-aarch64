0cb450d53dba4d8570d27c47be5f1e804e243bce
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 10:04:16 -0700

    Update menu.cpp

--- Android/app/src/main/cpp/menu/menu.cpp
+++ Android/app/src/main/cpp/menu/menu.cpp
@@ -1,46 +1,15 @@
-#include "menu.hpp"
+#include <jni.h>
 #include <android/log.h>
 
-#define LOG_TAG "MENU_HANDLER"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-
-MenuHandler::MenuHandler(JavaVM* vm, jobject activity)
-    : vm_(vm) {
-
+// Helper to ensure C++ can talk back to Java safely
+void ShowMenuNative(JavaVM* vm, jobject activityInstance) {
     JNIEnv* env = nullptr;
-    vm_->GetEnv((void**)&env, JNI_VERSION_1_6);
+    if (vm->GetEnv((void**)&env, JNI_VERSION_1_6) != JNI_OK) {
+        vm->AttachCurrentThread(&env, nullptr);
+    }
 
-    jclass cls = env->GetObjectClass(activity);
-    jfieldID fid = env->GetFieldID(
-        cls, "menuOverlay", "Landroid/widget/LinearLayout;");
-
-    jobject overlay = env->GetObjectField(activity, fid);
-    menuOverlayGlobal_ = env->NewGlobalRef(overlay);
-
-    LOGI("MenuHandler created");
-}
-
-MenuHandler::~MenuHandler() {
-    JNIEnv* env = nullptr;
-    vm_->GetEnv((void**)&env, JNI_VERSION_1_6);
-    env->DeleteGlobalRef(menuOverlayGlobal_);
-}
-
-void MenuHandler::setVisibility(bool visible) {
-    JNIEnv* env = nullptr;
-    vm_->AttachCurrentThread(&env, nullptr);
-
-    jclass viewCls = env->GetObjectClass(menuOverlayGlobal_);
-    jmethodID setVis = env->GetMethodID(viewCls, "setVisibility", "(I)V");
-
-    env->CallVoidMethod(menuOverlayGlobal_, setVis, visible ? 0 : 8);
-    visible_ = visible;
-}
-
-void MenuHandler::toggleVisibility() {
-    setVisibility(!visible_);
-}
-
-bool MenuHandler::isVisible() const {
-    return visible_;
+    if (env != nullptr) {
+        __android_log_print(ANDROID_LOG_INFO, "Menu", "Menu requested by Java Activity");
+        // Logic to trigger UI would go here
+    }
 }
