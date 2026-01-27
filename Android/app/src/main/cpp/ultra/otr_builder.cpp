46e48ae82ab6aac18b420422c9da6890ce018cae
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-26 19:49:18 -0700

    Update otr_builder.cpp

--- Android/app/src/main/cpp/ultra/otr_builder.cpp
+++ Android/app/src/main/cpp/ultra/otr_builder.cpp
@@ -1,49 +1,34 @@
 // Android/app/src/main/cpp/ultra/otr_builder.cpp
-#include "otr_builder.h"
 #include "assets_manifest.h"
 #include "rare_decompression.h"
 #include <vector>
 #include <fcntl.h>
 #include <unistd.h>
-#include <android/log.h>
 
-void run_native_otr_extraction(JNIEnv* env, jobject activity, int romFd, 
-                               uint8_t* manifestPtr, size_t manifestSize, 
-                               const char* outDirPath) {
-    
+void run_otr_generation(int romFd, uint8_t* manifestPtr, const char* outDirPath) {
     ManifestHeader* header = (ManifestHeader*)manifestPtr;
     AssetEntry* entries = (AssetEntry*)(manifestPtr + sizeof(ManifestHeader));
 
-    jclass activityClass = env->GetObjectClass(activity);
-    jmethodID updateMethod = env->GetMethodID(activityClass, "updateOtrProgress", "(ILjava/lang/String;)V");
-
     for (uint32_t i = 0; i < header->entryCount; i++) {
         AssetEntry& asset = entries[i];
-        
-        // Skip metadata/midis if defined in manifest as skip
         if (asset.type == ASSET_TYPE_SKIP) continue;
 
-        // 1. Seek and Read from ROM
+        [span_10](start_span)[span_11](start_span)// Logic from splat_inputs.py: Seek to ROM position[span_10](end_span)[span_11](end_span)
         lseek(romFd, asset.romOffset, SEEK_SET);
         std::vector<uint8_t> compBuffer(asset.compSize);
         read(romFd, compBuffer.data(), asset.compSize);
 
-        // 2. Decompress (Ported from rareunzip.py logic)
-        uint32_t actualDecompSize = 0;
-        uint8_t* decompData = decompress_rare_asset(compBuffer.data(), &actualDecompSize);
+        [span_12](start_span)// Logic from rareunzip.py: Decompress asset[span_12](end_span)
+        uint32_t actualSize = 0;
+        uint8_t* decompData = decompress_rare_asset(compBuffer.data(), &actualSize);
 
         if (decompData) {
-            // 3. Write to Android Internal Storage
-            std::string outPath = std::string(outDirPath) + "/" + asset.name;
-            int outFd = open(outPath.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0666);
-            write(outFd, decompData, actualDecompSize);
+            char fullPath[256];
+            snprintf(fullPath, sizeof(fullPath), "%s/%s", outDirPath, asset.name);
+            int outFd = open(fullPath, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            write(outFd, decompData, actualSize);
             close(outFd);
             free(decompData);
         }
-
-        // Update UI
-        jstring jName = env->NewStringUTF(asset.name);
-        env->CallVoidMethod(activity, updateMethod, i, jName);
-        env->DeleteLocalRef(jName);
     }
 }
