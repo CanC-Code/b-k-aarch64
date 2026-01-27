70d7460f00e10de06a7ae92097814b6f5c480372
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 00:06:24 -0700

    Update wrapper.cpp

--- Android/app/src/main/cpp/wrapper.cpp
+++ Android/app/src/main/cpp/wrapper.cpp
@@ -7,24 +7,9 @@ static JavaVM* g_vm = nullptr;
 
 /**
  * JNI_OnLoad is the standard entry point for a native library.
- * It is only defined here to prevent duplicate symbol errors.
  */
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
     g_vm = vm;
     __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "BKAWrapper Library Loaded");
     return JNI_VERSION_1_6;
 }
-
-/** * REMOVAL NOTICE:
- * The following JNI functions have been removed from this file:
- * - Java_com_bkawrapper_NativeBridge_nativeInit
- * - Java_com_bkawrapper_NativeBridge_runOtrGeneration
- * - Java_com_bkawrapper_NativeBridge_startGameLoop
- * - Java_com_bkawrapper_NativeBridge_pauseGameLoop
- * - Java_com_bkawrapper_NativeBridge_resumeGameLoop
- * - Java_com_bkawrapper_NativeBridge_cleanupGame
- * - Java_com_bkawrapper_NativeBridge_initTexture
- * - Java_com_bkawrapper_NativeBridge_updateTexture
- * * These are all fully implemented in NativeBridge.cpp. 
- * Defining them here caused the linker to fail with "duplicate symbol" errors.
- */
