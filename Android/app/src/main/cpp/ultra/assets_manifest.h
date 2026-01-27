5798588dcd6c292ad22bb88edce6de65819fb9bd
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 11:30:59 -0700

    Update assets_manifest.h

--- Android/app/src/main/cpp/ultra/assets_manifest.h
+++ Android/app/src/main/cpp/ultra/assets_manifest.h
@@ -3,19 +3,37 @@
 
 #include <stdint.h>
 
-#define ASSET_TYPE_SKIP 0
-#define ASSET_TYPE_RAW  1
-
-typedef struct {
-    uint32_t romOffset;
-    uint32_t compSize;
-    uint32_t type;
-    char name[128];
-} AssetEntry;
-
-typedef struct {
-    uint32_t magic;      // e.g., 0x424B4101
-    uint32_t entryCount;
-} ManifestHeader;
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+// Ensure the binary layout is exact to avoid crashes when reading from disk
+#pragma pack(push, 1)
+
+enum AssetType {
+    ASSET_TYPE_SKIP = 0,
+    ASSET_TYPE_COMPRESSED = 1,
+    ASSET_TYPE_RAW = 2
+};
+
+struct AssetEntry {
+    uint32_t romOffset;     // Offset in the ROM file
+    uint32_t compSize;      // Size of data in ROM
+    uint32_t decompSize;    // Expected size after decompression
+    uint32_t type;          // Maps to AssetType
+    char name[128];         // Increased to 128 to match common path lengths
+};
+
+struct ManifestHeader {
+    uint32_t magic;         // 'BKAM' (0x424B414D)
+    uint32_t entryCount;    // Total number of AssetEntry structs following header
+    uint32_t version;       // Versioning for future-proofing
+};
+
+#pragma pack(pop)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ASSETS_MANIFEST_H
