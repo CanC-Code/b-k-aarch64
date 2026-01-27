66ec6ce31454cdb267d6cf35e82aeb61f9a4a7de
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 10:02:12 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/ultra/NativeBridge.cpp
+++ Android/app/src/main/cpp/ultra/NativeBridge.cpp
@@ -1,18 +1,14 @@
-#include "NativeBridge.hpp"
+#include <jni.h>
 #include <android/log.h>
 
 #define LOG_TAG "NativeBridge"
 
-// Static member initialization
-JavaVM* NativeBridge::s_vm = nullptr;
-
-void NativeBridge::initialize(JavaVM* vm) {
-    s_vm = vm;
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "NativeBridge initialized with VM");
-}
+// REMOVED JNI_OnLoad from here to fix the "Duplicate Symbol" error.
+// The JNI_OnLoad in wrapper.cpp now handles the library startup.
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_NativeBridge_processFrame(JNIEnv* env, jobject thiz, jbyteArray data) {
-    // Implementation for frame processing
+    // This connects to the processFrame method in your NativeBridge.java
+    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "Processing frame data from Java");
 }
