6b381363467d90718c87377106373fe359696246
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 00:24:02 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/ultra/NativeBridge.cpp
+++ Android/app/src/main/cpp/ultra/NativeBridge.cpp
@@ -1,53 +1,30 @@
-#include <jni.h>
-#include <android/asset_manager.h>
-#include <android/asset_manager_jni.h>
-#include <android/log.h>
-#include "otr_builder.h"
-
-#define LOG_TAG "NativeBridge"
-
-static jobject g_mainActivityObj = nullptr;
-static jmethodID g_updateProgressMid = nullptr;
-
-extern "C" {
-
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject activity) {
-    if (g_mainActivityObj != nullptr) {
-        env->DeleteGlobalRef(g_mainActivityObj);
-    }
-    g_mainActivityObj = env->NewGlobalRef(activity);
-    jclass activityClass = env->GetObjectClass(g_mainActivityObj);
-    g_updateProgressMid = env->GetMethodID(activityClass, "updateOtrProgress", "(ILjava/lang/String;)V");
-}
+// Android/app/src/main/cpp/ultra/NativeBridge.cpp
 
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_NativeBridge_runOtrGeneration(JNIEnv* env, jclass clazz, 
                                                 jint romFd, 
                                                 jobject assetManager, 
                                                 jstring outputDir) {
-    const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
-    AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
-
-    AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
-    if (asset) {
-        uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
-        run_native_otr_generation_with_callback(env, g_mainActivityObj, g_updateProgressMid, 
-                                              romFd, manifestBuffer, outDir);
-        AAsset_close(asset);
-    } else {
-        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Could not find manifest_us.bin");
+    // DUPLICATE THE FD TO ENSURE NATIVE OWNERSHIP
+    int nativeFd = dup(romFd); 
+    if (nativeFd == -1) {
+        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Failed to duplicate FD");
+        return;
     }
 
+    const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
+    AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
+    AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
+
+    if (asset) {
+        uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
+        // PASS THE DUPLICATED FD
+        run_native_otr_generation_with_callback(env, g_mainActivityObj, g_updateProgressMid, 
+                                              nativeFd, manifestBuffer, outDir);
+        AAsset_close(asset);
+    }
+
+    // CLOSE THE NATIVE FD AFTER GENERATION IS COMPLETE
+    close(nativeFd); 
     env->ReleaseStringUTFChars(outputDir, outDir);
 }
-
-// Stub implementations for game loop controls
-JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_startGameLoop(JNIEnv* env, jclass clazz) { }
-JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_pauseGameLoop(JNIEnv* env, jclass clazz) { }
-JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_resumeGameLoop(JNIEnv* env, jclass clazz) { }
-JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_cleanupGame(JNIEnv* env, jclass clazz) { }
-JNIEXPORT jint JNICALL Java_com_bkawrapper_NativeBridge_initTexture(JNIEnv* env, jclass clazz) { return 0; }
-JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_updateTexture(JNIEnv* env, jclass clazz, jint tid) { }
-
-} // extern "C"
