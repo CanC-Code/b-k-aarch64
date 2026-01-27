e06a791a0e127d71491c0df65ab501cbb6e53b8f
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 01:48:05 -0700

    Update otr_builder.h

--- Android/app/src/main/cpp/ultra/otr_builder.h
+++ Android/app/src/main/cpp/ultra/otr_builder.h
@@ -4,21 +4,11 @@
 #include <jni.h>
 #include <stdint.h>
 
-// Define the structures missing in your previous build[span_3](end_span)
-struct AssetEntry {
-    char name[256];
-    uint32_t romOffset;
-    uint32_t compSize;
-    uint32_t type; 
-};
-
-struct ManifestHeader {
-    uint32_t entryCount;
-};
-
-#define ASSET_TYPE_SKIP 0
+// Removed duplicate AssetEntry and ManifestHeader structs
+// These are now exclusively defined in assets_manifest.h
 
 void otr_builder_set_jvm(JavaVM* vm);
+
 void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmethodID progressMid,
                                            int romFd, uint8_t* manifestPtr, const char* outDirPath);
 
