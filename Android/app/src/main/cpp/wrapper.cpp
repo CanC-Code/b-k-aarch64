812328ce68977ae907d1c1bc9f5c3cea65b8f37a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-26 21:41:22 -0700

    Update wrapper.cpp

--- Android/app/src/main/cpp/wrapper.cpp
+++ Android/app/src/main/cpp/wrapper.cpp
@@ -1,61 +1,30 @@
 #include <jni.h>
 #include <android/log.h>
-#include <android/asset_manager.h>
-#include <android/asset_manager_jni.h>
-#include <string>
-#include "ultra/otr_builder.h"
 
-#define LOG_TAG "BKAWrapper"
+#define LOG_TAG "BKAWrapper_Main"
 
 static JavaVM* g_vm = nullptr;
-static jobject g_mainActivityObj = nullptr;
-static jmethodID g_updateProgressMid = nullptr;
 
-// JNI_OnLoad is only defined here, so it's safe.
+/**
+ * JNI_OnLoad is the standard entry point for a native library.
+ * It is only defined here to prevent duplicate symbol errors.
+ */
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
     g_vm = vm;
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "BKAWrapper Library Loaded");
     return JNI_VERSION_1_6;
 }
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jobject thiz, jobject activity, jobject assetManager, jstring outputDir) {
-    g_mainActivityObj = env->NewGlobalRef(activity);
-    
-    jclass clazz = env->GetObjectClass(activity);
-    g_updateProgressMid = env->GetMethodID(clazz, "updateProgress", "(I)V");
-
-    const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
-    AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
-
-    // Load the binary manifest generated from your assets.yaml
-    AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
-    
-    if (asset) {
-        uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
-        
-        // This calls the actual logic in ultra/otr_builder.cpp
-        // Assuming romFd is handled or passed elsewhere, 
-        // or add a parameter if your Java side sends the FD.
-        int romFd = -1; 
-        run_native_otr_generation_with_callback(env, g_mainActivityObj, g_updateProgressMid, 
-                                              romFd, manifestBuffer, outDir);
-        
-        AAsset_close(asset);
-    } else {
-        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Could not find manifest_us.bin in assets");
-    }
-
-    env->ReleaseStringUTFChars(outputDir, outDir);
-}
-
-/** * DUPLICATE SYMBOLS REMOVED:
- * The following functions have been removed from this file because they are 
- * already implemented in ultra/NativeBridge.cpp:
- * - startGameLoop
- * - pauseGameLoop
- * - resumeGameLoop
- * - cleanupGame
- * - initTexture
- * - updateTexture
+/** * REMOVAL NOTICE:
+ * The following JNI functions have been removed from this file:
+ * - Java_com_bkawrapper_NativeBridge_nativeInit
+ * - Java_com_bkawrapper_NativeBridge_runOtrGeneration
+ * - Java_com_bkawrapper_NativeBridge_startGameLoop
+ * - Java_com_bkawrapper_NativeBridge_pauseGameLoop
+ * - Java_com_bkawrapper_NativeBridge_resumeGameLoop
+ * - Java_com_bkawrapper_NativeBridge_cleanupGame
+ * - Java_com_bkawrapper_NativeBridge_initTexture
+ * - Java_com_bkawrapper_NativeBridge_updateTexture
+ * * [span_4](start_span)[span_5](start_span)These are all fully implemented in NativeBridge.cpp[span_4](end_span)[span_5](end_span). 
+ * Defining them here caused the linker to fail with "duplicate symbol" errors.
  */
