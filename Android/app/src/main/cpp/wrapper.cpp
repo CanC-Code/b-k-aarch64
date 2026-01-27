c31285826c6685b3106167fb37b27b1366819f60
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-26 21:33:58 -0700

    Update wrapper.cpp

--- Android/app/src/main/cpp/wrapper.cpp
+++ Android/app/src/main/cpp/wrapper.cpp
@@ -1,95 +1,61 @@
 #include <jni.h>
-#include <thread>
-#include <atomic>
-#include <vector>
 #include <android/log.h>
-#include <unistd.h>
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+#include <string>
+#include "ultra/otr_builder.h"
 
-#define LOG_TAG "BK_WRAPPER"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOG_TAG "BKAWrapper"
 
-static std::atomic<bool> g_running{false};
-static std::atomic<bool> g_rom_loaded{false};
-static std::thread g_thread;
-static jobject g_activityObj = nullptr;
+static JavaVM* g_vm = nullptr;
+static jobject g_mainActivityObj = nullptr;
+static jmethodID g_updateProgressMid = nullptr;
 
-// External functions implemented in your emulator core or OTR builder
-extern "C" {
-    void core1_reset(uint8_t* ram);
-    void n_audioStep();
-    void core1_loadOTR(int fd); 
+// JNI_OnLoad is only defined here, so it's safe.
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
+    g_vm = vm;
+    return JNI_VERSION_1_6;
 }
 
-static std::vector<uint8_t> g_ram(8 * 1024 * 1024, 0);
-
-void emuLoop() {
-    // Wait until the OTR generation in NativeBridge.cpp signals it is done
-    while(!g_rom_loaded.load() && g_running.load()) {
-        usleep(100000); 
-    }
-
-    if (g_running.load()) {
-        core1_reset(g_ram.data());
-        LOGI("Emulator Loop Active");
-        while (g_running.load()) {
-            n_audioStep();
-            usleep(16000); // ~60fps target
-        }
-    }
-}
-
-extern "C" {
-
-// FIX: This is the ONLY definition of nativeInit in the project
+extern "C"
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject activity) {
-    if (g_activityObj != nullptr) {
-        env->DeleteGlobalRef(g_activityObj);
+Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jobject thiz, jobject activity, jobject assetManager, jstring outputDir) {
+    g_mainActivityObj = env->NewGlobalRef(activity);
+    
+    jclass clazz = env->GetObjectClass(activity);
+    g_updateProgressMid = env->GetMethodID(clazz, "updateProgress", "(I)V");
+
+    const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
+    AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
+
+    // Load the binary manifest generated from your assets.yaml
+    AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
+    
+    if (asset) {
+        uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
+        
+        // This calls the actual logic in ultra/otr_builder.cpp
+        // Assuming romFd is handled or passed elsewhere, 
+        // or add a parameter if your Java side sends the FD.
+        int romFd = -1; 
+        run_native_otr_generation_with_callback(env, g_mainActivityObj, g_updateProgressMid, 
+                                              romFd, manifestBuffer, outDir);
+        
+        AAsset_close(asset);
+    } else {
+        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Could not find manifest_us.bin in assets");
     }
-    g_activityObj = env->NewGlobalRef(activity);
-    LOGI("Native Bridge Initialized with Activity Ref");
+
+    env->ReleaseStringUTFChars(outputDir, outDir);
 }
 
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_startGameLoop(JNIEnv* env, jclass clazz) {
-    if (!g_running.load()) {
-        g_running.store(true);
-        // We assume the ROM is ready once this is called from Java
-        g_rom_loaded.store(true); 
-        g_thread = std::thread(emuLoop);
-    }
-}
-
-JNIEXPORT void JNICALL 
-Java_com_bkawrapper_NativeBridge_pauseGameLoop(JNIEnv* env, jclass clazz) {
-    // Implement pause logic if needed
-}
-
-JNIEXPORT void JNICALL 
-Java_com_bkawrapper_NativeBridge_resumeGameLoop(JNIEnv* env, jclass clazz) {
-    // Implement resume logic if needed
-}
-
-JNIEXPORT void JNICALL 
-Java_com_bkawrapper_NativeBridge_cleanupGame(JNIEnv* env, jclass clazz) {
-    g_running.store(false);
-    if(g_thread.joinable()) g_thread.join();
-    if(g_activityObj) {
-        env->DeleteGlobalRef(g_activityObj);
-        g_activityObj = nullptr;
-    }
-    LOGI("Native Bridge Cleaned Up");
-}
-
-// Graphics Stubs (implemented here to satisfy NativeBridge.java)
-JNIEXPORT jint JNICALL 
-Java_com_bkawrapper_NativeBridge_initTexture(JNIEnv* env, jclass clazz) { 
-    return 1; 
-}
-
-JNIEXPORT void JNICALL 
-Java_com_bkawrapper_NativeBridge_updateTexture(JNIEnv* env, jclass clazz, jint tid) {
-    // Texture update logic
-}
-
-} // extern "C"
+/** * DUPLICATE SYMBOLS REMOVED:
+ * The following functions have been removed from this file because they are 
+ * already implemented in ultra/NativeBridge.cpp:
+ * - startGameLoop
+ * - pauseGameLoop
+ * - resumeGameLoop
+ * - cleanupGame
+ * - initTexture
+ * - updateTexture
+ */
