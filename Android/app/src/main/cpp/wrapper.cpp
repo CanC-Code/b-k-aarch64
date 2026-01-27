5b2a15ef43355dc578d22d32b078bc6b93b4168e
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 11:12:17 -0700

    Update wrapper.cpp

--- Android/app/src/main/cpp/wrapper.cpp
+++ Android/app/src/main/cpp/wrapper.cpp
@@ -5,12 +5,6 @@
 
 extern "C" {
 
-// Library Entry Point - Only defined here
-jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "JNI_OnLoad: Library Loaded");
-    return JNI_VERSION_1_6;
-}
-
 // Matches: com.bkawrapper.MainActivity.initNative()
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_MainActivity_initNative(JNIEnv* env, jobject thiz) {
