f880bdd6a9ad927b7232270a290d1c863ca4056c
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 01:44:04 -0700

    Update otr_builder.cpp

--- Android/app/src/main/cpp/ultra/otr_builder.cpp
+++ Android/app/src/main/cpp/ultra/otr_builder.cpp
@@ -1,4 +1,6 @@
 #include "otr_builder.h"
+#include "assets_manifest.h"
+#include "../tools/rare_decompression.h"
 #include <vector>
 #include <thread>
 #include <unistd.h>
@@ -9,23 +11,24 @@
 #define LOG_TAG "OTR_BUILDER"
 
 static JavaVM* g_jvm = nullptr;
-void otr_builder_set_jvm(JavaVM* vm) { g_jvm = vm; [span_12](start_span)}
 
-void process_asset(int romFd, AssetEntry& asset, const char* outDirPath) {
+void otr_builder_set_jvm(JavaVM* vm) {
+    g_jvm = vm;
+}
+
+void process_asset(int romFd, AssetEntry asset, const char* outDirPath) {
     char path[512];
     snprintf(path, sizeof(path), "%s/%s", outDirPath, asset.name);
-    
+
     struct stat st;
     if (stat(path, &st) == 0 && st.st_size > 0) return; 
 
     std::vector<uint8_t> comp(asset.compSize);
+    // pread is essential here as multiple threads share the same romFd
     if (pread(romFd, comp.data(), asset.compSize, asset.romOffset) < (ssize_t)asset.compSize) return;
 
-    // External decompression call
-    extern uint8_t* decompress_rare_asset(uint8_t*, uint32_t, uint32_t*);
     uint32_t outSize = 0;
     uint8_t* decomp = decompress_rare_asset(comp.data(), asset.compSize, &outSize);
-    
     if (decomp) {
         int out = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
         if (out != -1) {
@@ -42,28 +45,28 @@ void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmet
     ManifestHeader* header = (ManifestHeader*)manifestPtr;
     AssetEntry* entries = (AssetEntry*)(manifestPtr + sizeof(ManifestHeader));
 
+    jobject globalActivity = env->NewGlobalRef(activity);
+
     const int batchSize = 4;
     for (uint32_t i = 0; i < header->entryCount; i += batchSize) {
         std::vector<std::thread> workers;
         for (int t = 0; t < batchSize && (i + t) < header->entryCount; t++) {
-            workers.emplace_back(process_asset, romFd, std::ref(entries[i + t]), outDirPath);
+            if (entries[i + t].type == ASSET_TYPE_SKIP) continue;
+            // Pass by value to avoid reference issues across threads
+            workers.emplace_back(process_asset, romFd, entries[i + t], outDirPath);
         }
+
         for (auto& w : workers) if (w.joinable()) w.join();
 
-        // ATTACH TO JVM FOR CALLBACK[span_12](end_span)
+        // Callback to Java UI
         JNIEnv* myEnv;
-        bool attached = false;
-        if (g_jvm->GetEnv((void**)&myEnv, JNI_VERSION_1_6) == JNI_EDETACHED) {
-            if (g_jvm->AttachCurrentThread(&myEnv, NULL) == JNI_OK) attached = true;
-        }
-
-        if (myEnv && activity && progressMid) {
+        if (g_jvm && g_jvm->GetEnv((void**)&myEnv, JNI_VERSION_1_6) == JNI_OK) {
             int percent = (int)((float)(i + 1) / header->entryCount * 100.0f);
             jstring jName = myEnv->NewStringUTF(entries[i].name);
-            [span_13](start_span)myEnv->CallVoidMethod(activity, progressMid, percent, jName);[span_13](end_span)
+            myEnv->CallVoidMethod(globalActivity, progressMid, percent, jName);
             myEnv->DeleteLocalRef(jName);
         }
-
-        [span_14](start_span)if (attached) g_jvm->DetachCurrentThread();[span_14](end_span)
     }
+    
+    env->DeleteGlobalRef(globalActivity);
 }
