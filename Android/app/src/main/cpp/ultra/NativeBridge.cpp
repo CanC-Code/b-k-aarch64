775bd61a727acfac9b23b56d1bdad1a67d4a9db5
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-26 21:07:40 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/ultra/NativeBridge.cpp
+++ Android/app/src/main/cpp/ultra/NativeBridge.cpp
@@ -2,37 +2,44 @@
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h>
 #include <android/log.h>
-#include "otr_builder.h"
 
-#define LOG_TAG "NativeBridge_OTR"
+#define LOG_TAG "NativeBridge"
+
+// Declare the external function from otr_builder.cpp
+extern "C" {
+    void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmethodID callback, 
+                                               int romFd, uint8_t* manifest, const char* outDir);
+}
+
+static jobject g_mainActivityObj = nullptr;
+static jmethodID g_updateProgressMid = nullptr;
 
 extern "C" {
 
-// runOtrGeneration is ONLY defined here.
+JNIEXPORT void JNICALL
+Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject activity) {
+    if (g_mainActivityObj != nullptr) env->DeleteGlobalRef(g_mainActivityObj);
+    g_mainActivityObj = env->NewGlobalRef(activity);
+    jclass activityClass = env->GetObjectClass(g_mainActivityObj);
+    g_updateProgressMid = env->GetMethodID(activityClass, "updateOtrProgress", "(ILjava/lang/String;)V");
+}
+
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_NativeBridge_runOtrGeneration(JNIEnv* env, jclass clazz, 
-                                                jint romFd, 
-                                                jobject assetManager, 
-                                                jstring outputDir) {
+                                                jint romFd, jobject assetManager, jstring outputDir) {
     const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
     AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
 
-    // Get the activity reference and method ID from wrapper.cpp's global storage
-    // or pass them in. For now, we assume otr_builder handles the callback.
-    
     AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
     if (asset) {
         uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
-        
-        // This function should be defined in your otr_builder.cpp
-        run_native_otr_generation(romFd, manifestBuffer, outDir);
-        
+        // FIX: Ensure this matches the declaration and the logic in otr_builder.cpp
+        run_native_otr_generation_with_callback(env, g_mainActivityObj, g_updateProgressMid, 
+                                              romFd, manifestBuffer, outDir);
         AAsset_close(asset);
     } else {
-        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Could not find manifest_us.bin in assets");
+        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Could not find manifest_us.bin");
     }
-
     env->ReleaseStringUTFChars(outputDir, outDir);
 }
-
-} // extern "C"
+}
