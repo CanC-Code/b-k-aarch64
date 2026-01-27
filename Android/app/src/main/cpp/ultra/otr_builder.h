92811167c907349b92c92fcad1bf89b16e8d9698
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 01:36:12 -0700

    Update otr_builder.h

--- Android/app/src/main/cpp/ultra/otr_builder.h
+++ Android/app/src/main/cpp/ultra/otr_builder.h
@@ -4,8 +4,21 @@
 #include <jni.h>
 #include <stdint.h>
 
-void otr_builder_set_jvm(JavaVM* vm);
+[span_3](start_span)// Define the structures missing in your previous build[span_3](end_span)
+struct AssetEntry {
+    char name[256];
+    uint32_t romOffset;
+    uint32_t compSize;
+    uint32_t type; 
+};
 
+struct ManifestHeader {
+    uint32_t entryCount;
+};
+
+#define ASSET_TYPE_SKIP 0
+
+void otr_builder_set_jvm(JavaVM* vm);
 void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmethodID progressMid,
                                            int romFd, uint8_t* manifestPtr, const char* outDirPath);
 
