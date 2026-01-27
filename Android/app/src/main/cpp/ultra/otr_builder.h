973ab829d5e57dd19d4bfca8a614f4b585e48be3
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 01:42:54 -0700

    Update otr_builder.h

--- Android/app/src/main/cpp/ultra/otr_builder.h
+++ Android/app/src/main/cpp/ultra/otr_builder.h
@@ -1,72 +1,25 @@
-#include "otr_builder.h"
-#include "assets_manifest.h"
-#include "../tools/rare_decompression.h"
-#include <vector>
-#include <thread>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <android/log.h>
+#ifndef OTR_BUILDER_H
+#define OTR_BUILDER_H
 
-#define LOG_TAG "OTR_BUILDER"
+#include <jni.h>
+#include <stdint.h>
 
-static JavaVM* g_jvm = nullptr;
+[span_3](start_span)// Define the structures missing in your previous build[span_3](end_span)
+struct AssetEntry {
+    char name[256];
+    uint32_t romOffset;
+    uint32_t compSize;
+    uint32_t type; 
+};
 
-void otr_builder_set_jvm(JavaVM* vm) {
-    g_jvm = vm;
-}
+struct ManifestHeader {
+    uint32_t entryCount;
+};
 
-void process_asset(int romFd, AssetEntry asset, const char* outDirPath) {
-    char path[512];
-    snprintf(path, sizeof(path), "%s/%s", outDirPath, asset.name);
+#define ASSET_TYPE_SKIP 0
 
-    struct stat st;
-    if (stat(path, &st) == 0 && st.st_size > 0) return; 
-
-    std::vector<uint8_t> comp(asset.compSize);
-    // pread is essential here as multiple threads share the same romFd
-    if (pread(romFd, comp.data(), asset.compSize, asset.romOffset) < (ssize_t)asset.compSize) return;
-
-    uint32_t outSize = 0;
-    uint8_t* decomp = decompress_rare_asset(comp.data(), asset.compSize, &outSize);
-    if (decomp) {
-        int out = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-        if (out != -1) {
-            write(out, decomp, outSize);
-            close(out);
-        }
-        free(decomp);
-    }
-}
-
+void otr_builder_set_jvm(JavaVM* vm);
 void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmethodID progressMid,
-                                           int romFd, uint8_t* manifestPtr, const char* outDirPath) {
-    mkdir(outDirPath, 0777);
-    ManifestHeader* header = (ManifestHeader*)manifestPtr;
-    AssetEntry* entries = (AssetEntry*)(manifestPtr + sizeof(ManifestHeader));
+                                           int romFd, uint8_t* manifestPtr, const char* outDirPath);
 
-    jobject globalActivity = env->NewGlobalRef(activity);
-
-    const int batchSize = 4;
-    for (uint32_t i = 0; i < header->entryCount; i += batchSize) {
-        std::vector<std::thread> workers;
-        for (int t = 0; t < batchSize && (i + t) < header->entryCount; t++) {
-            if (entries[i + t].type == ASSET_TYPE_SKIP) continue;
-            // Pass by value to avoid reference issues across threads
-            workers.emplace_back(process_asset, romFd, entries[i + t], outDirPath);
-        }
-
-        for (auto& w : workers) if (w.joinable()) w.join();
-
-        // Callback to Java UI
-        JNIEnv* myEnv;
-        if (g_jvm && g_jvm->GetEnv((void**)&myEnv, JNI_VERSION_1_6) == JNI_OK) {
-            int percent = (int)((float)(i + 1) / header->entryCount * 100.0f);
-            jstring jName = myEnv->NewStringUTF(entries[i].name);
-            myEnv->CallVoidMethod(globalActivity, progressMid, percent, jName);
-            myEnv->DeleteLocalRef(jName);
-        }
-    }
-    
-    env->DeleteGlobalRef(globalActivity);
-}
+#endif
