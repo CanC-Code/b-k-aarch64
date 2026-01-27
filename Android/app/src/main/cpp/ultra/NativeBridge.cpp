ee5270117436990b1a0e13ddb80ee7a3b5afd455
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-26 20:26:44 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/ultra/NativeBridge.cpp
+++ Android/app/src/main/cpp/ultra/NativeBridge.cpp
@@ -1,30 +1,23 @@
 #include <jni.h>
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h>
+#include <android/log.h>
 #include "otr_builder.h"
+
+#define LOG_TAG "NativeBridge"
 
 static jobject g_mainActivityObj = nullptr;
 static jmethodID g_updateProgressMid = nullptr;
 
-extern "C"
+extern "C" {
+
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject activity) {
-    // Save global reference to MainActivity to call UI updates later
     g_mainActivityObj = env->NewGlobalRef(activity);
     jclass activityClass = env->GetObjectClass(g_mainActivityObj);
     g_updateProgressMid = env->GetMethodID(activityClass, "updateOtrProgress", "(ILjava/lang/String;)V");
 }
 
-// Callback used by the OTR Builder to talk to Java
-void send_progress_to_java(int percent, const char* fileName) {
-    if (!g_mainActivityObj || !g_updateProgressMid) return;
-
-    // We need to attach the current thread to the JVM to perform the callback
-    // (Otr generation runs on a background thread in your Java code)
-    // Note: In a production environment, use a cached JavaVM* to get the correct Env.
-}
-
-extern "C"
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_NativeBridge_runOtrGeneration(JNIEnv* env, jclass clazz, 
                                                 jint romFd, 
@@ -33,17 +26,30 @@ Java_com_bkawrapper_NativeBridge_runOtrGeneration(JNIEnv* env, jclass clazz,
     const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
     AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
 
-    // 1. Load manifest.bin from APK assets
+    // Load the binary manifest generated from your assets.yaml
     AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
+    
     if (asset) {
         uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
         
-        // 2. Pass JNIEnv and GlobalRef to the builder so it can call updateOtrProgress
+        // Call the orchestrator in otr_builder.cpp
         run_native_otr_generation_with_callback(env, g_mainActivityObj, g_updateProgressMid, 
                                               romFd, manifestBuffer, outDir);
         
         AAsset_close(asset);
+    } else {
+        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Could not find manifest_us.bin in assets");
     }
 
     env->ReleaseStringUTFChars(outputDir, outDir);
 }
+
+// Game Loop and Texture Stubs
+JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_startGameLoop(JNIEnv* env, jclass clazz) { /* ... */ }
+JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_pauseGameLoop(JNIEnv* env, jclass clazz) { /* ... */ }
+JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_resumeGameLoop(JNIEnv* env, jclass clazz) { /* ... */ }
+JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_cleanupGame(JNIEnv* env, jclass clazz) { /* ... */ }
+JNIEXPORT jint JNICALL Java_com_bkawrapper_NativeBridge_initTexture(JNIEnv* env, jclass clazz) { return 0; }
+JNIEXPORT void JNICALL Java_com_bkawrapper_NativeBridge_updateTexture(JNIEnv* env, jclass clazz, jint tid) { /* ... */ }
+
+} // extern "C"
