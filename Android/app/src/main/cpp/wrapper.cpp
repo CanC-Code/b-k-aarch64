3cdad5ff2b0dd764a4c7414f574ee2fa7787aae2
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 10:21:25 -0700

    Update wrapper.cpp

--- Android/app/src/main/cpp/wrapper.cpp
+++ Android/app/src/main/cpp/wrapper.cpp
@@ -5,22 +5,16 @@
 
 extern "C" {
 
-// 1. Library Entry Point
+// Library Entry Point - Only defined here
 jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "JNI_OnLoad called");
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "JNI_OnLoad: Library Loaded");
     return JNI_VERSION_1_6;
 }
 
-// 2. MainActivity Link (Matches com.bkawrapper.MainActivity)
+// Matches: com.bkawrapper.MainActivity.initNative()
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_MainActivity_initNative(JNIEnv* env, jobject thiz) {
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Native: initNative called successfully");
-}
-
-// 3. NativeBridge Link (Matches com.bkawrapper.NativeBridge)
-JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_processFrame(JNIEnv* env, jobject thiz, jbyteArray data) {
-    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "Native: processFrame called");
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "MainActivity linked to native");
 }
 
 } // extern "C"
