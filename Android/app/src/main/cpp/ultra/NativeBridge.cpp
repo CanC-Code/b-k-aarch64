85edb7746c2b1ce3ddbf0a6b5fa12115f23968be
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 21:59:18 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/ultra/NativeBridge.cpp
+++ Android/app/src/main/cpp/ultra/NativeBridge.cpp
@@ -6,7 +6,7 @@
 
 #define LOG_TAG "NativeBridge"
 
-static jobject g_mainActivityObj = nullptr;
+static jobject g_callbackObj = nullptr; // Now points to the Service
 static jmethodID g_updateProgressMid = nullptr;
 
 extern "C" {
@@ -17,49 +17,37 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
 }
 
 JNIEXPORT void JNICALL
-Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject activity) {
-    if (g_mainActivityObj != nullptr) {
-        env->DeleteGlobalRef(g_mainActivityObj);
+Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject callbackTarget) {
+    if (g_callbackObj != nullptr) {
+        env->DeleteGlobalRef(g_callbackObj);
     }
-    g_mainActivityObj = env->NewGlobalRef(activity);
+    // callbackTarget is the OtrService instance
+    g_callbackObj = env->NewGlobalRef(callbackTarget);
 
-    jclass activityClass = env->GetObjectClass(g_mainActivityObj);
-    g_updateProgressMid = env->GetMethodID(activityClass, "updateOtrProgress", "(ILjava/lang/String;)V");
+    jclass serviceClass = env->GetObjectClass(g_callbackObj);
+    g_updateProgressMid = env->GetMethodID(serviceClass, "updateOtrProgress", "(ILjava/lang/String;)V");
 
-    if (g_updateProgressMid == nullptr) {
-        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Failed to find updateOtrProgress method!");
-    }
-
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "JNI Initialized");
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Bridge linked to Service");
 }
 
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_NativeBridge_runOtrGeneration(JNIEnv* env, jclass clazz,
-                                                jint romFd,
-                                                jobject assetManager,
-                                                jstring outputDir) {
-    if (g_mainActivityObj == nullptr || g_updateProgressMid == nullptr) {
-        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "NativeBridge not initialized properly");
-        return;
-    }
+                                                jint romFd, jobject assetManager, jstring outputDir) {
+    if (g_callbackObj == nullptr) return;
 
     const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
     AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
-
     AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
+
     if (asset) {
-        // Use a pointer cast to avoid ambiguity
         uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
         uint32_t manifestSize = (uint32_t)AAsset_getLength(asset);
 
-        run_native_otr_generation_with_callback(env, g_mainActivityObj, g_updateProgressMid,
+        run_native_otr_generation_with_callback(env, g_callbackObj, g_updateProgressMid,
                                               romFd, manifestBuffer, manifestSize, outDir);
         AAsset_close(asset);
-    } else {
-        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Manifest not found in assets");
     }
-
     env->ReleaseStringUTFChars(outputDir, outDir);
 }
 
-} // extern "C"
+}
