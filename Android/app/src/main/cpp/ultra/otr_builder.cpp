9eaf97a07437f7903ebe93ec092050c63e171c78
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 21:59:39 -0700

    Update otr_builder.cpp

--- Android/app/src/main/cpp/ultra/otr_builder.cpp
+++ Android/app/src/main/cpp/ultra/otr_builder.cpp
@@ -1,29 +1,20 @@
 #include "otr_builder.h"
 #include <android/log.h>
 #include <string.h>
-#include <stdlib.h>
-
-#define LOG_TAG "OtrBuilder"
-
-static JavaVM* g_vm = nullptr;
 
 extern "C" {
+static JavaVM* g_vm = nullptr;
+void otr_builder_set_jvm(JavaVM* vm) { g_vm = vm; }
 
-void otr_builder_set_jvm(JavaVM* vm) {
-    g_vm = vm;
-}
-
-void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmethodID progressMid,
+void run_native_otr_generation_with_callback(JNIEnv* env, jobject callbackObj, jmethodID progressMid,
                                            int romFd, uint8_t* manifestPtr, uint32_t manifestSize, 
                                            const char* outDirPath) {
     
-    if (!manifestPtr || manifestSize < 48) return;
-
     uint32_t entryCount = manifestSize / 48;
-    
+
     for (uint32_t i = 0; i < entryCount; i++) {
-        // 1. Manage JNI local references to prevent overflow crash
-        if (env->PushLocalFrame(10) < 0) break; 
+        // PREVENT CRASH: Push a local frame to clean up strings automatically
+        if (env->PushLocalFrame(10) < 0) return;
 
         uint8_t* record = manifestPtr + (i * 48);
         char fileName[33];
@@ -31,18 +22,12 @@ void run_native_otr_generation_with_callback(JNIEnv* env, jobject activity, jmet
         fileName[32] = '\0';
 
         int percentage = (int)((i * 100) / entryCount);
-
-        // 2. Call Java
         jstring jName = env->NewStringUTF(fileName);
-        env->CallVoidMethod(activity, progressMid, percentage, jName);
+        
+        // Call the Service's update method
+        env->CallVoidMethod(callbackObj, progressMid, percentage, jName);
 
-        // 3. Pop the frame (automatically deletes jName)
-        env->PopLocalFrame(NULL);
+        env->PopLocalFrame(NULL); // Cleans up jName immediately
     }
-
-    // Final Update
-    jstring doneMsg = env->NewStringUTF("Done");
-    env->CallVoidMethod(activity, progressMid, 100, doneMsg);
 }
-
-} // extern "C"
+}
