6fb2f2669eaf330b32de915e4d6a137b02fb9f32
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 22:07:37 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/ultra/NativeBridge.cpp
+++ Android/app/src/main/cpp/ultra/NativeBridge.cpp
@@ -6,7 +6,7 @@
 
 #define LOG_TAG "NativeBridge"
 
-static jobject g_callbackObj = nullptr; // Now points to the Service
+static jobject g_callbackObj = nullptr;
 static jmethodID g_updateProgressMid = nullptr;
 
 extern "C" {
@@ -21,30 +21,27 @@ Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject c
     if (g_callbackObj != nullptr) {
         env->DeleteGlobalRef(g_callbackObj);
     }
-    // callbackTarget is the OtrService instance
     g_callbackObj = env->NewGlobalRef(callbackTarget);
 
     jclass serviceClass = env->GetObjectClass(g_callbackObj);
     g_updateProgressMid = env->GetMethodID(serviceClass, "updateOtrProgress", "(ILjava/lang/String;)V");
-
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Bridge linked to Service");
 }
 
 JNIEXPORT void JNICALL
 Java_com_bkawrapper_NativeBridge_runOtrGeneration(JNIEnv* env, jclass clazz,
                                                 jint romFd, jobject assetManager, jstring outputDir) {
-    if (g_callbackObj == nullptr) return;
+    if (g_callbackObj == nullptr || g_updateProgressMid == nullptr) return;
 
     const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
     AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
     AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
 
     if (asset) {
-        uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
-        uint32_t manifestSize = (uint32_t)AAsset_getLength(asset);
+        uint8_t* buffer = (uint8_t*)AAsset_getBuffer(asset);
+        uint32_t size = (uint32_t)AAsset_getLength(asset);
 
         run_native_otr_generation_with_callback(env, g_callbackObj, g_updateProgressMid,
-                                              romFd, manifestBuffer, manifestSize, outDir);
+                                              romFd, buffer, size, outDir);
         AAsset_close(asset);
     }
     env->ReleaseStringUTFChars(outputDir, outDir);
