84d69219c2ad9ad4660a32a3377d04745653426e
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 21:55:41 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/ultra/NativeBridge.cpp
+++ Android/app/src/main/cpp/ultra/NativeBridge.cpp
@@ -26,6 +26,10 @@ Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject a
     jclass activityClass = env->GetObjectClass(g_mainActivityObj);
     g_updateProgressMid = env->GetMethodID(activityClass, "updateOtrProgress", "(ILjava/lang/String;)V");
 
+    if (g_updateProgressMid == nullptr) {
+        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Failed to find updateOtrProgress method!");
+    }
+
     __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "JNI Initialized");
 }
 
@@ -34,11 +38,17 @@ Java_com_bkawrapper_NativeBridge_runOtrGeneration(JNIEnv* env, jclass clazz,
                                                 jint romFd,
                                                 jobject assetManager,
                                                 jstring outputDir) {
+    if (g_mainActivityObj == nullptr || g_updateProgressMid == nullptr) {
+        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "NativeBridge not initialized properly");
+        return;
+    }
+
     const char* outDir = env->GetStringUTFChars(outputDir, nullptr);
     AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
 
     AAsset* asset = AAssetManager_open(mgr, "manifest_us.bin", AASSET_MODE_BUFFER);
     if (asset) {
+        // Use a pointer cast to avoid ambiguity
         uint8_t* manifestBuffer = (uint8_t*)AAsset_getBuffer(asset);
         uint32_t manifestSize = (uint32_t)AAsset_getLength(asset);
 
