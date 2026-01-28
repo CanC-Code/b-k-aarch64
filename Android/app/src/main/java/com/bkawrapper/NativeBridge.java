14163718de83a203cf8baece4fc74b7317634a3e
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 19:27:33 -0700

    Update NativeBridge.java

--- Android/app/src/main/java/com/bkawrapper/NativeBridge.java
+++ Android/app/src/main/java/com/bkawrapper/NativeBridge.java
@@ -4,35 +4,9 @@ import android.content.Context;
 import android.content.res.AssetManager;
 
 public class NativeBridge {
-    static {
-        // This MUST match the name in CMakeLists.txt: add_library(bkawrapper ...)
-        System.loadLibrary("bkawrapper");
-    }
+    // Matches: Java_com_bkawrapper_NativeBridge_nativeInit
+    public static native void nativeInit(Object activity);
 
-    public interface OtrCompletionListener {
-        void onOtrComplete();
-    }
-
-    private static OtrCompletionListener completionListener;
-
-    public static void setOtrCompletionListener(OtrCompletionListener listener) {
-        completionListener = listener;
-    }
-
-    // --- Native Methods ---
-    public static native void nativeInit(Context activity);
+    // Matches: Java_com_bkawrapper_NativeBridge_runOtrGeneration
     public static native void runOtrGeneration(int romFd, AssetManager assetManager, String outputDir);
-    public static native void startGameLoop();
-    public static native void pauseGameLoop();
-    public static native void resumeGameLoop();
-    public static native void cleanupGame();
-    public static native int initTexture();
-    public static native void updateTexture(int tid);
-
-    // --- Callback for C++ ---
-    public static void notifyFinished() {
-        if (completionListener != null) {
-            completionListener.onOtrComplete();
-        }
-    }
 }
