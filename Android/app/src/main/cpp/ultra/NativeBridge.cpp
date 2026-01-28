9107c3211969b9ba48f22180d77ff28057b4311d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 19:27:54 -0700

    Update NativeBridge.cpp

--- Android/app/src/main/cpp/ultra/NativeBridge.cpp
+++ Android/app/src/main/cpp/ultra/NativeBridge.cpp
@@ -10,8 +10,7 @@ static jobject g_mainActivityObj = nullptr;
 static jmethodID g_updateProgressMid = nullptr;
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "JNI_OnLoad: Library Loading...");
-    otr_builder_set_jvm(vm); // Initialize the VM for otr_builder
+    otr_builder_set_jvm(vm); 
     return JNI_VERSION_1_6;
 }
 
@@ -22,13 +21,14 @@ Java_com_bkawrapper_NativeBridge_nativeInit(JNIEnv* env, jclass clazz, jobject a
     if (g_mainActivityObj != nullptr) {
         env->DeleteGlobalRef(g_mainActivityObj);
     }
-    // Store global reference to avoid garbage collection of the activity during extraction
+    // Vital: NewGlobalRef prevents the activity from being GC'd while we use it
     g_mainActivityObj = env->NewGlobalRef(activity);
-    
+
     jclass activityClass = env->GetObjectClass(g_mainActivityObj);
+    // Find the updateOtrProgress method on the MainActivity
     g_updateProgressMid = env->GetMethodID(activityClass, "updateOtrProgress", "(ILjava/lang/String;)V");
     
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Native Bridge Initialized");
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "JNI initialized with GlobalRef");
 }
 
 JNIEXPORT void JNICALL
@@ -45,11 +45,9 @@ Java_com_bkawrapper_NativeBridge_runOtrGeneration(JNIEnv* env, jclass clazz,
         run_native_otr_generation_with_callback(env, g_mainActivityObj, g_updateProgressMid,
                                               romFd, manifestBuffer, outDir);
         AAsset_close(asset);
-    } else {
-        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "Failed to load manifest_us.bin");
     }
 
     env->ReleaseStringUTFChars(outputDir, outDir);
 }
 
-} // extern "C"
+}
