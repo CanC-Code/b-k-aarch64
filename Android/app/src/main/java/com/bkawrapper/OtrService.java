41389f8850d6c5f718b62d8bcd917b137bec430d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-27 21:58:20 -0700

    Update OtrService.java

--- Android/app/src/main/java/com/bkawrapper/OtrService.java
+++ Android/app/src/main/java/com/bkawrapper/OtrService.java
@@ -5,24 +5,42 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
 import android.net.Uri;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class OtrService extends Service {
+    private static OtrService instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    // This is the method C++ will call
+    public void updateOtrProgress(int percent, String status) {
+        Intent intent = new Intent("OTR_PROGRESS");
+        intent.putExtra("percent", percent);
+        intent.putExtra("status", status);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String uriString = intent.getStringExtra("uri");
         String outDir = intent.getStringExtra("outDir");
         Uri uri = Uri.parse(uriString);
 
-        // Run in a background thread to prevent ANR
         new Thread(() -> {
             try (ParcelFileDescriptor pfd = getContentResolver().openFileDescriptor(uri, "r")) {
                 if (pfd != null) {
+                    // Initialize the bridge to point to THIS service instance
+                    NativeBridge.nativeInit(this);
                     NativeBridge.runOtrGeneration(pfd.getFd(), getAssets(), outDir);
-                    NativeBridge.notifyFinished();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            instance = null;
             stopSelf();
         }).start();
 
