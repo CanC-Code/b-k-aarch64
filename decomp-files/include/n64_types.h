2d854761ba964bb9c3e383464c15454e231e4aa9
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-12 17:48:45 -0600

    Update n64_types.h

--- decomp-files/include/n64_types.h
+++ decomp-files/include/n64_types.h
@@ -1,5 +1,6 @@
 #ifndef _N64_TYPES_H_
 #define _N64_TYPES_H_
+
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
@@ -17,7 +18,7 @@ typedef volatile uint32_t vu32;
   #define FALSE 0
 #endif
 
-// 2. SDK Guards
+// 2. SDK Guards (Complete Lockout of original MIPS headers)
 #define __OS_H__
 #define __OS_THREAD_H__
 #define __OS_MESSAGE_H__
@@ -30,44 +31,57 @@ typedef volatile uint32_t vu32;
 #define _GU_H_
 #define _BOOL_H_ 
 
-// 3. The "Acmd" Structure
-typedef struct { unsigned int w0; unsigned int w1; } Acmd_words;
-typedef union { Acmd_words words; long long force_align; } Acmd;
-
-// 4. OS Stubs & Macros
-typedef s32  OSPri;
-typedef s32  OSId;
+// 3. System Structures & IO
 typedef void* OSMesg;
-typedef s32  OSHWIntr;
 typedef struct { void* mtqueue; void* fullqueue; s32 validCount; s32 first; s32 msgCount; OSMesg* msg; } OSMesgQueue;
 typedef struct { uint8_t d[128]; } OSThread;
 typedef struct { uint8_t d[64];  } OSContPad;
-typedef struct { uint8_t d[128]; } OSPfs;
 typedef struct { uint8_t d[32];  } OSIoMesg;
+typedef s32  OSHWIntr;
+typedef u32  OSIntMask;
 
-#define OS_IM_NONE 0
-typedef u32 OSIntMask;
-#define K0_TO_PHYS(x) ((u32)(uintptr_t)(x))
-static inline u32 osVirtualToPhysical(void* vaddr) { return (u32)(uintptr_t)vaddr; }
-
-// 5. Audio/Video Constants
-#define ADPCMFSIZE      16
-#define UNITY_PITCH     0x8000
-#define OS_TV_NTSC      0
+#define OS_MESG_BLOCK 1
+#define OS_TV_NTSC    0
+#define OS_IM_NONE    0
 extern s32 osTvType;
 
-// 6. Graphics Types
+// 4. Audio Synthesis States (The "Missing Link")
+// We define these as arrays so they decay into the pointers the engine expects.
+typedef int16_t ADPCM_STATE[16];
+typedef int16_t RESAMPLE_STATE[16];
+typedef int16_t POLEF_STATE[4];
+typedef int16_t ENVMIX_STATE[40];
+
+// 5. Audio Commands & Constants
+#define ADPCMFSIZE      16
+#define UNITY_PITCH     0x8000
+#define A_INIT          0x01
+#define A_CONTINUE      0x02
+#define A_LOOP          0x02
+#define A_MAIN          0x04
+#define A_AUX           0x08
+#define A_VOL           0x10
+#define A_RATE          0x20
+#define A_SETVOL        0x03
+#define A_ENVMIXER      0x04
+#define A_RESAMPLE      0x06
+
+// 6. Graphics Pipeline Types
 typedef uint64_t Gfx;
+typedef uint64_t Acmd;
 typedef struct { int32_t m[4][4]; } Mtx;
-typedef struct { uint8_t d[16];  } Vtx; // RESTORED VTX
+typedef struct { uint8_t d[16];  } Vtx;
 typedef struct { uint8_t d[32];  } LookAt;
 typedef struct { uint8_t d[32];  } Hilite;
 typedef struct { uint8_t d[32];  } Light;
-typedef struct { uint8_t d[64];  } PositionalLight;
 
-typedef int16_t ADPCM_STATE[16];
+// 7. Hardware Interaction Macros (Stubbed for ARM)
+#define K0_TO_PHYS(x) ((u32)(uintptr_t)(x))
+static inline u32 osVirtualToPhysical(void* vaddr) { return (u32)(uintptr_t)vaddr; }
+#define _SHIFTL(v, s, w) ((unsigned int)(((unsigned int)(v) & ((0x01 << (w)) - 1)) << (s)))
 
 #ifndef bcopy
   #define bcopy(src, dst, n) __builtin_memmove(dst, src, n)
 #endif
+
 #endif
