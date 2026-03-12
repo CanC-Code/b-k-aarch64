2bffe3a2e3d6715aefbd21f521cc79e2bf71c663
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-12 17:40:45 -0600

    Update n64_types.h

--- decomp-files/include/n64_types.h
+++ decomp-files/include/n64_types.h
@@ -17,7 +17,8 @@ typedef volatile uint32_t vu32;
   #define FALSE 0
 #endif
 
-// 2. SDK Guards
+// 2. SDK Guards (Complete Lockout)
+#define __OS_H__
 #define __OS_THREAD_H__
 #define __OS_MESSAGE_H__
 #define __OS_CONT_H__
@@ -29,88 +30,55 @@ typedef volatile uint32_t vu32;
 #define _GU_H_
 #define _BOOL_H_
 
-// 3. The "Acmd" Structure
-typedef struct { unsigned int w0; unsigned int w1; } Acmd_words;
-typedef union { Acmd_words words; long long force_align; } Acmd;
-
-// 4. OS Stubs & Messaging
-typedef s32  OSPri;
-typedef s32  OSId;
+// 3. IO & Messaging (Fixes code_1D00.c)
 typedef void* OSMesg;
+typedef struct { void* mtqueue; void* fullqueue; s32 validCount; s32 first; s32 msgCount; OSMesg* msg; } OSMesgQueue;
+typedef struct { uint8_t d[32]; } OSIoMesg; // Missing type restored
 typedef s32  OSHWIntr;
+#define OS_MESG_BLOCK 1
+#define OS_TV_NTSC    0
+extern s32 osTvType;
 
-typedef struct {
-    void* mtqueue;
-    void* fullqueue;
-    s32           validCount; 
-    s32           first;
-    s32           msgCount;
-    OSMesg* msg;
-} OSMesgQueue;
+// 4. Graphics Pipe (GBI/RDP) Constants (Fixes code_15B30.c)
+#define G_RM_NOOP           0
+#define G_RM_NOOP2          0
+#define G_ZBUFFER           0x00000001
+#define G_SHADE             0x00000004
+#define G_CULL_BOTH         0x00003000
+#define G_FOG               0x00010000
+#define G_LIGHTING          0x00020000
+#define G_TEXTURE_GEN       0x00040000
+#define G_TEXTURE_GEN_LINEAR 0x00080000
+#define G_LOD               0x00100000
+#define G_SHADING_SMOOTH    0x00200000
+#define G_PM_NPRIMITIVE     0
+#define G_AC_NONE           0
+#define G_CD_MAGICSQ        0
+#define G_SC_NON_INTERLACE  0
+#define G_IM_FMT_RGBA       0
+#define G_IM_SIZ_16b        1
+#define G_CYC_1CYCLE        0
+#define G_TC_FILT           0
 
-typedef struct { uint8_t d[128]; } OSThread;
-typedef struct { uint8_t d[64];  } OSContPad;
-typedef struct { uint8_t d[128]; } OSPfs;
-
-#ifndef OS_IM_NONE
-  #define OS_IM_NONE 0
-#endif
-
-#define OS_MESG_NOBLOCK 0
-#define OS_MESG_BLOCK   1
-#define PFS_ERR_ID_FATAL 1
-#define PFS_ERR_DEVICE   2
-
-typedef u32 OSIntMask;
-#define K0_TO_PHYS(x) ((u32)(uintptr_t)(x))
-static inline u32 osVirtualToPhysical(void* vaddr) { return (u32)(uintptr_t)vaddr; }
-static inline OSIntMask osGetIntMask(void) { return 0; }
-static inline OSIntMask osSetIntMask(OSIntMask m) { (void)m; return 0; }
-static inline s32 osSendMesg(OSMesgQueue *q, OSMesg m, s32 flag) { return 0; }
-static inline s32 osRecvMesg(OSMesgQueue *q, OSMesg *m, s32 flag) { return 0; }
-
-// 5. Audio Commands & Constants
-#define ADPCMFSIZE      16
-#define ADPCMVSIZE      8
-#define LFSAMPLES       4
-#define UNITY_PITCH     0x8000
-#define MAX_RATIO       2
-
-#define A_INIT          0x01
-#define A_CONTINUE      0x02
-#define A_LOOP          0x02
-#define A_MAIN          0x04
-#define A_AUX           0x08
-#define A_VOL           0x10
-#define A_RATE          0x20
-#define A_LEFT          0x40
-#define A_RIGHT         0x80
-#define A_LOADBUFF      0x01
-#define A_ADPCM         0x02
-#define A_SETVOL        0x03
-#define A_ENVMIXER      0x04
-#define A_POLEF         0x05
-#define A_RESAMPLE      0x06
-#define A_SAVEBUFF      0x07
-#define A_LOADADPCM     0x08
-#define A_NOAUX         0x10
-
-// 6. Graphics & Helper Types
-typedef uint64_t Gfx;
-typedef struct { int32_t m[4][4]; } Mtx;
-typedef struct { uint8_t d[16];  } Vtx;
-typedef struct { uint8_t d[32];  } LookAt;
-typedef struct { uint8_t d[32];  } Hilite;
-typedef struct { uint8_t d[32];  } Light;
-typedef struct { uint8_t d[64];  } PositionalLight;
-
+// 5. Audio Pipeline & States
+#define ADPCMFSIZE 16
+#define UNITY_PITCH 0x8000
 typedef int16_t ADPCM_STATE[16];
 typedef int16_t RESAMPLE_STATE[16];
 typedef int16_t POLEF_STATE[4];
 typedef int16_t ENVMIX_STATE[40];
 
-#define _SHIFTL(v, s, w) ((unsigned int)(((unsigned int)(v) & ((0x01 << (w)) - 1)) << (s)))
-#define _SHIFTR(v, s, w) ((unsigned int)(((unsigned int)(v) >> (s)) & ((0x01 << (w)) - 1)))
+// 6. Common Types & Structs
+typedef uint64_t Gfx;
+typedef uint64_t Acmd;
+typedef struct { int32_t m[4][4]; } Mtx;
+typedef struct { uint8_t d[64]; } PositionalLight;
+
+// 7. Dummy SDK Functions (Forwarding to nothing)
+#define gDPSetRenderMode(p, m, m2) 
+#define gSPClearGeometryMode(p, m)
+#define gSPSetGeometryMode(p, m)
+#define gSPTexture(p, s, t, l, f, d)
 
 #ifndef bcopy
   #define bcopy(src, dst, n) __builtin_memmove(dst, src, n)
