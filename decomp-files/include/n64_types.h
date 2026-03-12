573dd400f8182dea7ca39f50d2fb6d8898f5429c
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-12 16:53:49 -0600

    Update n64_types.h

--- decomp-files/include/n64_types.h
+++ decomp-files/include/n64_types.h
@@ -17,7 +17,7 @@ typedef volatile uint32_t vu32;
   #define FALSE 0
 #endif
 
-// 2. SDK Guards: Pre-emptively locking SDK headers
+// 2. SDK Guards
 #define __OS_THREAD_H__
 #define __OS_MESSAGE_H__
 #define __OS_CONT_H__
@@ -27,28 +27,36 @@ typedef volatile uint32_t vu32;
 #define _GU_H_
 #define _BOOL_H_
 
-// 3. OS Stubs
+// 3. The "Acmd" Structure (Fixes n_abi.h errors)
+typedef struct {
+    unsigned int w0;
+    unsigned int w1;
+} Acmd_words;
+
+typedef union {
+    Acmd_words words;
+    long long  force_structure_alignment;
+} Acmd;
+
+// 4. OS Stubs & Macros
 typedef s32  OSPri;
 typedef s32  OSId;
 typedef void* OSMesg;
 typedef struct { uint8_t d[48];  } OSMesgQueue;
 typedef struct { uint8_t d[128]; } OSThread;
 typedef struct { uint8_t d[64];  } OSContPad;
-typedef struct { uint8_t d[32];  } OSViMode;
 
-// 4. OS Macros & Stubs
 #define OS_IM_NONE 0
 typedef u32 OSIntMask;
+#define K0_TO_PHYS(x) ((u32)(uintptr_t)(x))
 static inline u32 osVirtualToPhysical(void* vaddr) { return (u32)(uintptr_t)vaddr; }
 static inline OSIntMask osGetIntMask(void) { return 0; }
 static inline OSIntMask osSetIntMask(OSIntMask m) { (void)m; return 0; }
 
-// 5. Audio Codec Constants (Required for load.c / n_adpcm.c)
+// 5. Audio Pipeline Constants
 #define ADPCMFSIZE      16
 #define ADPCMVSIZE      8
 #define LFSAMPLES       4
-
-// 6. Audio Command Macros
 #define A_INIT          0x01
 #define A_CONTINUE      0x02
 #define A_LOOP          0x02
@@ -61,23 +69,25 @@ static inline OSIntMask osSetIntMask(OSIntMask m) { (void)m; return 0; }
 #define A_LOADBUFF      0x01
 #define A_ADPCM         0x02
 
-// 7. Graphics & Audio Opaque Types
+// 6. Graphics & Helper Types
 typedef uint64_t Gfx;
-typedef uint64_t Acmd;
 typedef struct { int32_t m[4][4]; } Mtx;
 typedef struct { uint8_t d[16];  } Vtx;
 typedef struct { uint8_t d[32];  } LookAt;
 typedef struct { uint8_t d[32];  } Hilite;
 typedef struct { uint8_t d[32];  } Light;
+typedef struct { uint8_t d[64];  } PositionalLight;
 
-// Audio States defined as arrays to allow automatic pointer decay
 typedef int16_t ADPCM_STATE[16];
 typedef int16_t RESAMPLE_STATE[16];
 typedef int16_t POLEF_STATE[4];
 typedef int16_t ENVMIX_STATE[40];
 
+// N64 bit-shifting helpers
+#define _SHIFTL(v, s, w) ((unsigned int)(((unsigned int)(v) & ((0x01 << (w)) - 1)) << (s)))
+#define _SHIFTR(v, s, w) ((unsigned int)(((unsigned int)(v) >> (s)) & ((0x01 << (w)) - 1)))
+
 #ifndef bcopy
   #define bcopy(src, dst, n) __builtin_memmove(dst, src, n)
 #endif
-
 #endif
