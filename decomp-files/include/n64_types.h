fc3247425aa2521cd2dc307c6cbf0334dac138dc
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-12 18:57:27 -0600

    Update n64_types.h

--- decomp-files/include/n64_types.h
+++ decomp-files/include/n64_types.h
@@ -5,12 +5,12 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-// 1. Basic N64 Primitives
-typedef int8_t   s8;  typedef uint8_t  u8;
-typedef int16_t  s16; typedef uint16_t u16;
-typedef int32_t  s32; typedef uint32_t u32;
-typedef int64_t  s64; typedef uint64_t u64;
-typedef float    f32; typedef double   f64;
+// 1. Basic N64 Primitives (64-bit Safe)
+typedef int8_t s8;   typedef uint8_t u8;
+typedef int16_t s16; typedef uint16_t u16;
+typedef int32_t s32; typedef uint32_t u32;
+typedef int64_t s64; typedef uint64_t u64;
+typedef float f32;   typedef double f64;
 typedef volatile uint32_t vu32;
 
 #ifndef TRUE
@@ -18,7 +18,7 @@ typedef volatile uint32_t vu32;
   #define FALSE 0
 #endif
 
-// 2. SDK Guards
+// 2. SDK Guards: Prevent any original header from loading
 #define __OS_H__
 #define __OS_THREAD_H__
 #define __OS_MESSAGE_H__
@@ -29,15 +29,15 @@ typedef volatile uint32_t vu32;
 #define _LIBAUDIO_H_
 #define _N_LIBAUDIO_H_
 #define _GU_H_
+#define _SP_H_
 #define _ULTRATYPES_H_
 
-// 3. Audio State Arrays
-typedef int16_t ADPCM_STATE[16];
-typedef int16_t RESAMPLE_STATE[16];
-typedef int16_t POLEF_STATE[4];
-typedef int16_t ENVMIX_STATE[40];
+// 3. Command Lists (Acmd & Gfx)
+typedef uint64_t Gfx;
+typedef struct { unsigned int w0, w1; } Acmd_words;
+typedef union { Acmd_words words; long long align; } Acmd;
 
-// 4. Detailed Audio Structure Emulation
+// 4. Audio Emulation (AL) Structure Layouts
 typedef s32 ALMicroTime;
 typedef s32 ALPan;
 typedef void* ALDMAproc;
@@ -45,7 +45,7 @@ typedef void* ALDMANew;
 typedef struct { uint8_t d[32]; } ALHeap;
 typedef struct ALLink_s { struct ALLink_s *next; struct ALLink_s *prev; } ALLink;
 
-// Structures for bnkf.c (Audio Bank Loading)
+// Structures for sound banking
 typedef struct { uint8_t d[32]; } ALADPCMBook;
 typedef struct { uint8_t d[32]; } ALADPCMloop;
 typedef struct { uint8_t d[32]; } ALRawLoop;
@@ -57,63 +57,41 @@ typedef struct {
     } waveInfo;
     u8 *base;
     u32 len;
-    u8 type;
-    u8 flags;
+    u8 type, flags;
 } ALWaveTable;
 
-typedef struct {
-    uint8_t d[32]; // Opaque envelope
-} ALEnvelope;
+typedef struct { ALWaveTable *wavetable; u8 flags; void* envelope; void* keyMap; } ALSound;
+typedef struct { s32 instCount; ALSound *instArray[1]; } ALBank;
+typedef struct { s32 bankCount; ALBank *bankArray[1]; } ALBankFile;
+typedef struct { s16 seqCount; s32 seqArray[1]; } ALSeqFile;
 
-typedef struct {
-    uint8_t d[32]; // Opaque keymap
-} ALKeyMap;
-
-typedef struct {
-    ALEnvelope *envelope;
-    ALKeyMap   *keyMap;
-    ALWaveTable *wavetable;
-    u8 flags;
-} ALSound;
-
-typedef struct {
-    s16 seqCount;
-    s32 seqArray[1]; 
-} ALSeqFile;
-
-// ALEvent Union (Fixes code_21AF0.c)
+// ALEvent Union (Fixes the midi/unk18 member errors)
 typedef struct {
     s16 type;
+    s32 ticks;
     union {
-        struct { u8 status; u8 byte1; u8 byte2; } midi;
-        struct { void *data; } unk18;
+        struct { u8 status, byte1, byte2; } midi;
+        struct { void *data; u32 unk0, unk4; } unk18;
         s32 i;
     } msg;
 } ALEvent;
 
 typedef struct { void *evtq; } ALCSPlayer;
-typedef struct { uint8_t d[64]; } ALSynConfig;
+typedef ALCSPlayer N_ALSeqPlayer;
 typedef struct { uint8_t d[64]; } ALSynth;
+typedef ALSynth N_ALSynth;
 
-// 5. System & Graphics
-typedef uint64_t Gfx;
-typedef struct { int32_t m[4][4]; } Mtx;
-typedef struct { uint8_t d[16]; } Vtx;
-typedef struct { uint8_t d[64]; } LookAt;
-typedef struct { uint8_t d[64]; } Hilite;
-typedef struct { uint8_t d[64]; } Light;
-typedef struct { uint8_t d[128]; } uSprite;
-
+// 5. System Stubs
 typedef void* OSMesg;
-typedef struct { void* mt; void* full; int validCount; } OSMesgQueue;
+typedef struct { void* mt; void* full; int count; } OSMesgQueue;
+typedef struct { uint8_t d[256]; } OSThread;
+typedef struct { uint8_t d[64];  } OSContPad;
 
-// 6. Macros & Inlines
+// 6. Constants
 #define OS_IM_NONE 0
 #define ADPCMFSIZE 16
-#define ADPCMVSIZE 8
 #define UNITY_PITCH 0x8000
-#define AL_ADPCM_WAVE 0
-#define AL_RAW16_WAVE 1
+#define AL_BANK_VERSION 0x424c // 'BL'
 #define AL_SEQP_MIDI_EVT 2
 #define AL_MIDI_ControlChange 3
 #define AL_UNK18_EVT 18
