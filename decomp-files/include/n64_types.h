ed9c37e617181e25369f30899254c4df84a12b6f
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-12 19:01:11 -0600

    Update n64_types.h

--- decomp-files/include/n64_types.h
+++ decomp-files/include/n64_types.h
@@ -5,12 +5,12 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-// 1. Basic N64 Primitives (64-bit Safe)
-typedef int8_t s8;   typedef uint8_t u8;
-typedef int16_t s16; typedef uint16_t u16;
-typedef int32_t s32; typedef uint32_t u32;
-typedef int64_t s64; typedef uint64_t u64;
-typedef float f32;   typedef double f64;
+// 1. Basic N64 Primitives
+typedef int8_t   s8;  typedef uint8_t  u8;
+typedef int16_t  s16; typedef uint16_t u16;
+typedef int32_t  s32; typedef uint32_t u32;
+typedef int64_t  s64; typedef uint64_t u64;
+typedef float    f32; typedef double   f64;
 typedef volatile uint32_t vu32;
 
 #ifndef TRUE
@@ -18,7 +18,7 @@ typedef volatile uint32_t vu32;
   #define FALSE 0
 #endif
 
-// 2. SDK Guards: Prevent any original header from loading
+// 2. SDK Lockdown: Prevent legacy headers from redefining these
 #define __OS_H__
 #define __OS_THREAD_H__
 #define __OS_MESSAGE_H__
@@ -26,77 +26,38 @@ typedef volatile uint32_t vu32;
 #define _GBI_H_
 #define _ABI_H_
 #define _MBI_H_
-#define _LIBAUDIO_H_
-#define _N_LIBAUDIO_H_
 #define _GU_H_
 #define _SP_H_
 #define _ULTRATYPES_H_
+#define _BOOL_H_ 
 
-// 3. Command Lists (Acmd & Gfx)
+// 3. Command Lists & Graphics Structures
 typedef uint64_t Gfx;
-typedef struct { unsigned int w0, w1; } Acmd_words;
-typedef union { Acmd_words words; long long align; } Acmd;
+typedef struct { int32_t m[4][4]; } Mtx;
+typedef struct { float m[4][4]; } MtxF;
+typedef struct { uint8_t d[16]; } Vtx;
 
-// 4. Audio Emulation (AL) Structure Layouts
-typedef s32 ALMicroTime;
-typedef s32 ALPan;
-typedef void* ALDMAproc;
-typedef void* ALDMANew;
+// 4. Audio Processing States (Crucial for bnkf.c and synthInternals.h)
+typedef int16_t ADPCM_STATE[16];
+typedef int16_t RESAMPLE_STATE[16];
+typedef int16_t POLEF_STATE[4];
+typedef int16_t ENVMIX_STATE[40];
+
+// 5. Audio Library (AL) Stubs
 typedef struct { uint8_t d[32]; } ALHeap;
-typedef struct ALLink_s { struct ALLink_s *next; struct ALLink_s *prev; } ALLink;
+typedef struct { uint8_t d[32]; } ALSynConfig;
+typedef struct { s32 ticks; s32 type; union { s32 i; void *p; } msg; } ALEvent;
 
-// Structures for sound banking
-typedef struct { uint8_t d[32]; } ALADPCMBook;
-typedef struct { uint8_t d[32]; } ALADPCMloop;
-typedef struct { uint8_t d[32]; } ALRawLoop;
-
-typedef struct {
-    union {
-        struct { ALADPCMBook *book; ALADPCMloop *loop; } adpcmWave;
-        struct { ALRawLoop *loop; } rawWave;
-    } waveInfo;
-    u8 *base;
-    u32 len;
-    u8 type, flags;
-} ALWaveTable;
-
-typedef struct { ALWaveTable *wavetable; u8 flags; void* envelope; void* keyMap; } ALSound;
-typedef struct { s32 instCount; ALSound *instArray[1]; } ALBank;
-typedef struct { s32 bankCount; ALBank *bankArray[1]; } ALBankFile;
-typedef struct { s16 seqCount; s32 seqArray[1]; } ALSeqFile;
-
-// ALEvent Union (Fixes the midi/unk18 member errors)
-typedef struct {
-    s16 type;
-    s32 ticks;
-    union {
-        struct { u8 status, byte1, byte2; } midi;
-        struct { void *data; u32 unk0, unk4; } unk18;
-        s32 i;
-    } msg;
-} ALEvent;
-
-typedef struct { void *evtq; } ALCSPlayer;
-typedef ALCSPlayer N_ALSeqPlayer;
-typedef struct { uint8_t d[64]; } ALSynth;
-typedef ALSynth N_ALSynth;
-
-// 5. System Stubs
+// 6. System Stubs
 typedef void* OSMesg;
 typedef struct { void* mt; void* full; int count; } OSMesgQueue;
-typedef struct { uint8_t d[256]; } OSThread;
+typedef struct { uint8_t d[128]; } OSThread;
 typedef struct { uint8_t d[64];  } OSContPad;
 
-// 6. Constants
-#define OS_IM_NONE 0
-#define ADPCMFSIZE 16
-#define UNITY_PITCH 0x8000
-#define AL_BANK_VERSION 0x424c // 'BL'
-#define AL_SEQP_MIDI_EVT 2
-#define AL_MIDI_ControlChange 3
-#define AL_UNK18_EVT 18
-
+// 7. 64-bit Pointer Virtualization
+#define K0_TO_PHYS(x) ((u32)(uintptr_t)(x))
 static inline uint32_t osVirtualToPhysical(void* vaddr) { return (u32)(uintptr_t)vaddr; }
+
 #ifndef bcopy
   #define bcopy(src, dst, n) __builtin_memmove(dst, src, n)
 #endif
