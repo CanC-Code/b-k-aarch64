99c5bdbf95883437582fd3f3ba1de1b2cda1b0a6
CanC-Code <CanC-Code@users.noreply.github.com>
2026-08-05 23:44:38 -0600

    Add missing print/font stubs for linker
    Revert print.c exclusion (broke build) and add stubs for all
    print/font symbols referenced by other game code. The real print.c
    has 64-bit pointer truncation bugs in sprite processing that need
    to be fixed before it can be used.

--- Android/app/src/main/cpp/ultra/print_stubs.c
+++ Android/app/src/main/cpp/ultra/print_stubs.c
@@ -3,3 +3,13 @@ void print_init(void) {}
 void print_getLettersFromFont(void* arg0, void* arg1) {}
 void func_802E5F38(void) {}
 void func_802E5F10(void) {}
+void print_free(void) {}
+void print_updateBoldLetterFontDelayedFreeing(void) {}
+void print_bold_spaced(void* a, void* b, int c, int d, int e, int f) {}
+void print_bold_overlapping(void* a, void* b, int c, int d, int e, int f) {}
+void print_setBoldFontTexture(int a) {}
+void print_resetBoldFontTexture(void) {}
+void text_setNormalTextColor(int r, int g, int b, int a) {}
+void print_dialog_gradient2(void* a, void* b, int c, int d, int e, int f, int g, int h) {}
+void print_dialog(void* a, void* b, int c, int d, int e, int f) {}
+void print_freeBoldLetterFont(void) {}
