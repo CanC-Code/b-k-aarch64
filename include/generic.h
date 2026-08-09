ff86f5d679106eb2f101f17730d9fa08ad1e52e8
CanC-Code <CanC-Code@users.noreply.github.com>
2026-08-08 23:19:31 -0600

    Revert "Fix Generic callback types: s32 -> void* for first parameter"
    This reverts commit 8f9b4e29101913958a391b4052e3f88c475e2acd.

--- include/generic.h
+++ include/generic.h
@@ -3,9 +3,9 @@
 
 typedef void (* GenFunction_0)(void);
 typedef void (* GenFunction_1)(s32);
-typedef void (* GenFunction_2)(void*, s32);
-typedef void (* GenFunction_3)(void*, s32, s32);
-typedef void (* GenFunction_4)(void*, s32, s32, s32);
+typedef void (* GenFunction_2)(s32, s32);
+typedef void (* GenFunction_3)(s32, s32, s32);
+typedef void (* GenFunction_4)(s32, s32, s32, s32);
 typedef void (* GenFunction_5)(s32, s32, s32, s32, s32);
 typedef void (* GenFunction_6)(void *);
 
