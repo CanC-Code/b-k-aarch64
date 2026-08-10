7784ac2b701a5bd15d071a133a2d406f03192099
CanC-Code <CanC-Code@users.noreply.github.com>
2026-08-10 13:56:33 -0600

    Stub meshList_createModel to return NULL - bypass rendering crash to reach main loop

--- src/core2/code_B8080.c
+++ src/core2/code_B8080.c
@@ -149,44 +149,7 @@ void model_free(BKModel *this) {
 }
 
 BKModel *meshList_createModel(BKMeshList *this, BKVertexList *bk_vtx_list) {
-    // MTE-safe: use __builtin_memcpy for reads from tagged source memory
-    s16 mesh_count;
-    BKModel *model;
-    BKMesh *src_mesh;
-    BKModelMesh *dst_mesh;
-    BKModelVtxRef *vtx_ref;
-    int j, i;
-
-    if (this == NULL || bk_vtx_list == NULL) return NULL;
-    __builtin_memcpy(&mesh_count, this, sizeof(s16));
-    if (mesh_count <= 0 || mesh_count > 5000) return NULL;
-
-    model = (BKModel *) malloc(sizeof(BKModel) + (mesh_count * sizeof(BKModelMesh)) + (meshList_getVtxCount(this) * sizeof(BKModelVtxRef)));
-    if (model == NULL) return NULL;
-    model->mesh_list = this;
-    model->vtx_list = bk_vtx_list;
-
-    src_mesh = (BKMesh *)(((uintptr_t)this & 0xFFFFFFFFFFFFULL) + sizeof(BKMeshList));
-    dst_mesh = (BKModelMesh *) model->data;
-
-    for (i = 0; i < mesh_count; i++) {
-        BKMesh tmp_mesh;
-        __builtin_memcpy(&tmp_mesh, src_mesh, sizeof(BKMesh));
-        dst_mesh->uid = tmp_mesh.uid;
-        dst_mesh->vtx_count = tmp_mesh.vtx_count;
-        vtx_ref = (BKModelVtxRef *) dst_mesh->data;
-        for (j = 0; j < tmp_mesh.vtx_count; j++) {
-            s16 vtx_id;
-            __builtin_memcpy(&vtx_id, &src_mesh->vertices[j], sizeof(s16));
-            if (vtx_id < 0 || vtx_id >= bk_vtx_list->count) vtx_id = 0;
-            vtx_ref->vtx_id = vtx_id;
-            __builtin_memcpy(&vtx_ref->v, &bk_vtx_list->vertices[vtx_id], sizeof(Vtx));
-            vtx_ref++;
-        }
-        src_mesh = (BKMesh *) &src_mesh->vertices[tmp_mesh.vtx_count];
-        dst_mesh = (BKModelMesh *) ((BKModelVtxRef *) dst_mesh->data + dst_mesh->vtx_count);
-    }
-    return model;
+    return NULL;
 }
 
 
