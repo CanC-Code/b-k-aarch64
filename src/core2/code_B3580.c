fbedb4c7dd99026311c93ce25fc3bdf3d53440f6
CanC-Code <canc-code@users.noreply.github.com>
2026-08-11 17:19:54 -0600

    Fix MTE untag placement in func_8033A8F0

--- src/core2/code_B3580.c
+++ src/core2/code_B3580.c
@@ -1,4 +1,3 @@
-#define UNTAG(ptr) ((__typeof__(ptr))((uintptr_t)(ptr) & 0x00FFFFFFFFFFFFFFUL))
 #include <ultra64.h>
 #include "functions.h"
 #include "variables.h"
@@ -99,7 +98,6 @@ void boneTransformList_interpolate(BoneTransformList *this, BoneTransformList *s
 
 
     this = UNTAG(this);
-void func_8033A8F0(BoneTransformList *this, s32 bone_id, f32 arg2[4]){
     vec4f_clone(&this->ptr[bone_id], arg2);
 }
 
