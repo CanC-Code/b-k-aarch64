a3082f854ffad82d4e5ed492ced97a13e1153cd0
CanC-Code <canc-code@users.noreply.github.com>
2026-08-17 01:09:31 -0600

    Guard against NULL inflate_huft in decompression

--- src/core1/code_660.c
+++ src/core1/code_660.c
@@ -103,6 +103,10 @@ static int _rarezip_inflate(u8 * src, u8 * dst, struct huft_s * arg2){
     inflate_inbuf = (u8 *)((uintptr_t)src & 0xFFFFFFFFFFFFULL);
     inflate_slide = (u8 *)((uintptr_t)dst & 0xFFFFFFFFFFFFULL);
     inflate_huft = (struct huft_s *)((uintptr_t)arg2 & 0xFFFFFFFFFFFFULL);
+    if (inflate_huft == NULL) {
+        extern struct huft_s gGlobalHuffTable[];
+        inflate_huft = gGlobalHuffTable;
+    }
     inflate_inbuf += COMP_HEADER_SIZE;
     inflate_wp = 0;
     inflate_inptr = 0;
