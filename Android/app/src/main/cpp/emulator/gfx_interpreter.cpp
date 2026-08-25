55e480948d00056823454da606b1017fcf5602a6
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-08-25 11:04:59 -0600

    Remove duplicate default label

--- Android/app/src/main/cpp/emulator/gfx_interpreter.cpp
+++ Android/app/src/main/cpp/emulator/gfx_interpreter.cpp
@@ -951,7 +951,7 @@ void RSP_ProcessGfxTask(OSTask* tp) {
                 break;
 
             default:
-default:
+
                 if (s_frameCount <= 3)
                     __android_log_print(ANDROID_LOG_WARN, "BKA_GFX",
                         "Unhandled op=0x%02X at cmd %zu", opcode, total - 1);
