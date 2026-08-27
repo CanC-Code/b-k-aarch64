fae162e1ea23745af97871ea323ddc83136c1da5
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-08-26 22:47:09 -0600

    Fix GfxCommand to 8 bytes and stride to 8

--- Android/app/src/main/cpp/emulator/gfx_interpreter.cpp
+++ Android/app/src/main/cpp/emulator/gfx_interpreter.cpp
@@ -998,7 +998,7 @@ void RSP_ProcessGfxTask(OSTask* tp) {
 
     DListFrame stack[64];
     int depth = 0;
-    size_t current_stride = 16;
+    size_t current_stride = 8;
     size_t stack_stride[64];
     uint8_t *cur = (uint8_t*)tp->t.data_ptr;
     uint8_t *cur_end = cur + tp->t.data_size;
@@ -1027,7 +1027,7 @@ void RSP_ProcessGfxTask(OSTask* tp) {
         }
 
         GfxCommand c = {0};
-        memcpy(&c, cur, 16);
+        memcpy(&c, cur, 8);
         uint8_t opcode = GFX_OPCODE(c);
 
         // Per-command logging disabled for performance
