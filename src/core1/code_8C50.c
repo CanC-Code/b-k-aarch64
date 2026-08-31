cd087c2248da927c815d7ec58e343c54f713a650
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-08-31 11:55:51 -0600

    Restore asynchronous graphics task queue and thread5 startup

--- src/core1/code_8C50.c
+++ src/core1/code_8C50.c
@@ -552,7 +552,7 @@ void thread5_create(void) {
     sGfxTask.t.yield_data_ptr = (u64 *) yield_data_ptr;
 
     osCreateThread(&sThread5, THREAD5_ID,thread5_entry, NULL, STACK_START(sThread5Stack), THREAD5_PRI);
-    // osStartThread(&sThread5); // disabled for synchronous HLE
+    osStartThread(&sThread5);
 }
 
 void thread5_enableControllerTimer(void) {
