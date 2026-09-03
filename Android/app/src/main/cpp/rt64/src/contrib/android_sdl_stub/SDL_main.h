8b930562b9465bd30d201a9d5f6ead3cb1e26cd9
ccvo <cardscollectiblesandoddities@gmail.com>
2026-09-03 13:40:23 -0600

    Add all missing SDL stubs for Android

---
+++ Android/app/src/main/cpp/rt64/src/contrib/android_sdl_stub/SDL_main.h
@@ -0,0 +1,8 @@
+#ifndef RT64_ANDROID_SDL_STUB_SDL_MAIN_H
+#define RT64_ANDROID_SDL_STUB_SDL_MAIN_H
+
+// Minimal SDL stub for Android compilation.
+typedef struct SDL_Window SDL_Window;
+typedef void* SDL_GLContext;
+
+#endif // RT64_ANDROID_SDL_STUB_SDL_MAIN_H
