cbcdbaed35d1a31019a1e8522d5c754d7f3d81e6
ccvo <cardscollectiblesandoddities@gmail.com>
2026-09-16 23:37:54 -0600

    diag: dump first 64 bytes of every RSP task data_ptr

--- Android/app/src/main/cpp/emulator/stubs.cpp
+++ Android/app/src/main/cpp/emulator/stubs.cpp
@@ -504,6 +504,24 @@ void osSpTaskStartGo(OSTask *tp) {
                 tp->t.data_ptr, tp->t.data_size,
                 d ? *(uint32_t*)(d + 0) : 0,
                 d ? *(uint32_t*)(d + ((tp->t.data_size >= 16) ? tp->t.data_size - 16 : 0)) : 0);
+            if (d) {
+                __android_log_print(ANDROID_LOG_ERROR, "BKA_GFX",
+                    "TASKBYTES [0-15]  %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X",
+                    d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7],
+                    d[8],d[9],d[10],d[11],d[12],d[13],d[14],d[15]);
+                __android_log_print(ANDROID_LOG_ERROR, "BKA_GFX",
+                    "TASKBYTES [16-31] %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X",
+                    d[16],d[17],d[18],d[19],d[20],d[21],d[22],d[23],
+                    d[24],d[25],d[26],d[27],d[28],d[29],d[30],d[31]);
+                __android_log_print(ANDROID_LOG_ERROR, "BKA_GFX",
+                    "TASKBYTES [32-47] %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X",
+                    d[32],d[33],d[34],d[35],d[36],d[37],d[38],d[39],
+                    d[40],d[41],d[42],d[43],d[44],d[45],d[46],d[47]);
+                __android_log_print(ANDROID_LOG_ERROR, "BKA_GFX",
+                    "TASKBYTES [48-63] %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X %02X%02X%02X%02X",
+                    d[48],d[49],d[50],d[51],d[52],d[53],d[54],d[55],
+                    d[56],d[57],d[58],d[59],d[60],d[61],d[62],d[63]);
+            }
         }
         RSP_ProcessGfxTask(tp);
         HLE_TriggerN64Event(OS_EVENT_SP);
