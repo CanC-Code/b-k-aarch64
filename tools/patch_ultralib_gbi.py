41e93c1fd103e1691ee95c493f0a0ed9ce33fa6d
ccvo <cardscollectiblesandoddities@gmail.com>
2026-09-17 13:46:34 -0600

    Fix CI: Update patch_ultralib_gbi.py to find gbi.h dynamically

--- tools/patch_ultralib_gbi.py
+++ tools/patch_ultralib_gbi.py
@@ -1,26 +1,27 @@
-import os
-
-gbi_path = "include/ultra64/gbi.h"
+import os, glob
+files = glob.glob("include/**/gbi.h", recursive=True)
+if not files:
+    print("gbi.h not found, skipping patch.")
+    exit(0)
+gbi_path = files[0]
 with open(gbi_path, "r") as f:
     content = f.read()
-
-# Make sure we don't double patch
 if "BKA_LOG_GDL_EMIT" not in content:
     replacement = """
-#define BKA_LOG_GDL_EMIT(dl) do { \\
-    static int emit_cnt = 0; \\
-    if (emit_cnt++ < 20) { \\
-        BKA_LOG("gSPDisplayList EMIT target=0x%08X caller_ra=%p", (uint32_t)(dl), __builtin_return_address(0)); \\
-    } \\
+#define BKA_LOG_GDL_EMIT(dl) do { \
+    static int emit_cnt = 0; \
+    if (emit_cnt++ < 20) { \
+        BKA_LOG("gSPDisplayList EMIT target=0x%08X caller_ra=%p", (uint32_t)(dl), __builtin_return_address(0)); \
+    } \
 } while(0)
 
-#define gSPDisplayList(pkt, dl) \\
-{ \\
-    BKA_LOG_GDL_EMIT(dl); \\
-    gDma1p((pkt), G_DL, (dl), 0, 0); \\
+#define gSTDisplayList(pkt, dl) \
+{ \
+    BKA_LOG_GDL_EMIT(dl); \
+    gDma1p((pkt), G_DL, (dl), 0, 0); \
 }
 """
-    content = content.replace("#define gSPDisplayList(pkt, dl) \\", replacement + "\n//#define gSPDisplayList(pkt, dl) \\")
-    
+    content = content.replace("#define gSPDisplayList(pkt, dl) \\\n{", replacement + "\n//#define gSTDisplayList(pkt, dl) \\\n//{")
     with open(gbi_path, "w") as f:
         f.write(content)
+    print("Patched " + gbi_path)
