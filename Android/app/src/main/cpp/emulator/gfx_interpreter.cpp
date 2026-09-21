cca5682e5264de87cf570d88190ce6eaa679f7e6
ccvo <cardscollectiblesandoddities@gmail.com>
2026-09-21 10:31:40 -0600

    Raise MAX_TOTAL_CMDS to 20000 for testing

--- Android/app/src/main/cpp/emulator/gfx_interpreter.cpp
+++ Android/app/src/main/cpp/emulator/gfx_interpreter.cpp
@@ -1988,7 +1988,7 @@ void RSP_ProcessGfxTask(OSTask* tp) {
     uint8_t *cur = (uint8_t*)tp->t.data_ptr;
     uint8_t *cur_end = cur + tp->t.data_size;
 
-    const size_t MAX_TOTAL_CMDS = 3000;   /* real RSP tasks are <2000 cmds */
+    const size_t MAX_TOTAL_CMDS = 20000;   /* real RSP tasks are <2000 cmds */
     const size_t MAX_DL_CMDS = 5000;
     size_t total = 0;
     size_t dl_cmds = 0;
